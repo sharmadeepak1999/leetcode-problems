fbc04e13b9e236647ea37133e72d31346e17f9cc
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-12 21:14:43 +0530

    Time: 7 ms (31.07%), Space: 8.7 MB (91.98%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.cpp
+++ 832-flipping-an-image/832-flipping-an-image.cpp
@@ -6,9 +6,11 @@ public:
         for(int i = 0; i < rows; i++) {
             int j = 0, k = columns - 1;
             while(j <= k) {
-                int temp = !image[i][j];
-                image[i][j] = !image[i][k];
-                image[i][k] = temp;
+                if(image[i][j] == image[i][k]) {
+                    int temp = !image[i][j];
+                    image[i][j] = !image[i][k];
+                    image[i][k] = temp;
+                }
                 j++;
                 k--;
             }
