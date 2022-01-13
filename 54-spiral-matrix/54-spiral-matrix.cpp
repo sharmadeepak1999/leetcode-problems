61c2c98d37db11d29a494861ef3925ef8439cb27
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-13 22:02:04 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (71.23%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -3,32 +3,28 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int row = matrix.size(), col = matrix[0].size();
         vector<int> res;
-        int c = row * col;
-        int currRow = 0, currCol = col - 1, colStart = 0, colEnd = col - 1, rowStart = 1, rowEnd = row - 1;
-        while(c >= 1) {
-            for(int i = colStart; c >= 1 && i <= colEnd; i++) {
-                res.push_back(matrix[currRow][i]);
-                c--;
+        int colStart = 0, colEnd = col - 1, rowStart = 0, rowEnd = row - 1;
+        while(rowStart <= rowEnd && colStart <= colEnd) {
+            for(int i = colStart; i <= colEnd; i++) {
+                res.push_back(matrix[rowStart][i]);
             }
-            for(int j = rowStart; c >= 1 && j <= rowEnd; j++) {
-                res.push_back(matrix[j][currCol]);
-                c--;
+            rowStart++;
+            for(int j = rowStart; j <= rowEnd; j++) {
+                res.push_back(matrix[j][colEnd]);
             }
             colEnd--;
-            for(int k = colEnd; c >= 1 && k >= colStart; k--) {
-                res.push_back(matrix[row - currRow - 1][k]);
-                c--;
+            if(rowStart <= rowEnd) {
+                for(int k = colEnd; k >= colStart; k--) {
+                    res.push_back(matrix[rowEnd][k]);
+                }
+                rowEnd--;   
             }
-            rowEnd--;
-            for(int l = rowEnd; c >= 1 && l >= rowStart; l--) {
-                res.push_back(matrix[l][col - currCol - 1]);
-                c--;
+            if(colStart <= colEnd) {
+                for(int l = rowEnd; l >= rowStart; l--) {
+                    res.push_back(matrix[l][colStart]);
+                }
+                colStart++;
             }
-            currRow++;
-            currCol--;
-            colStart++;
-            rowStart++;
-            cout << c;
         }
         return res;
     }
