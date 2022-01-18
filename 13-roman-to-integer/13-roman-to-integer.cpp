b97b9a001b471a406c3674130914704feb2f1fc7
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-18 20:58:04 +0530

    Time: 45 ms (5.12%), Space: 7.8 MB (45.60%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,43 +2,24 @@ class Solution {
 public:
     int romanToInt(string s) {
         int num = 0;
+        int i = 0;
         int len = s.length();
-        for(int i = 0; i < len; i++) {
-            if(s[i] == 'I') {
-                if(i + 1 < len && s[i + 1] == 'V') {
-                    num += 4;
-                    i++;
-                } else if(i + 1 < len && s[i + 1] == 'X'){
-                    num += 9;
-                    i++;
-                } else {
-                    num++;
-                }
-            } else if(s[i] == 'X') {
-                if(i + 1 < len && s[i + 1] == 'L') {
-                    num += 40;
-                    i++;
-                } else if(i + 1 < len && s[i + 1] == 'C') {
-                    num += 90;
-                    i++;
-                } else {
-                    num += 10;
-                }
-            } else if(s[i] == 'C') {
-                if(i + 1 < len && s[i + 1] == 'D') {
-                    num += 400;
-                    i++;
-                } else if(i + 1 < len && s[i + 1] == 'M') {
-                    num += 900;
-                    i++;
-                } else {
-                    num += 100;
-                }
-            } else if(s[i] == 'V') num += 5;
-            else if(s[i] == 'L') num += 50;
-            else if(s[i] == 'D') num += 500;
-            else if(s[i] == 'M') num += 1000;
+        unordered_map<char, int> hash = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        while(i < len - 1) {
+            int curr = hash[s[i]], next = hash[s[i + 1]];
+            if(curr < next) num -= curr;
+            else num += curr;
+            i++;
         }
+        num += hash[s[i]];
         return num;
     }
 };
