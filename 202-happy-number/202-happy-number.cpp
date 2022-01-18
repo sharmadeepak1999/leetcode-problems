6756560ddbaefd61c14dedd0ce38ce45b9687842
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-18 22:33:26 +0530

    Time: 4 ms (40.52%), Space: 5.9 MB (64.22%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    
+    int nextNum(int n) {
+        int temp = n;
+        n = 0;
+        while(temp > 0) {
+            int d = temp % 10;
+            n += d * d;
+            temp /= 10;
+        }
+        return n;
+    }
     bool isHappy(int n) {
-        set<int> s;
-        while(n != 1) {
-            int temp = n;
-            n = 0;
-            while(temp > 0) {
-                int d = temp % 10;
-                n += d * d;
-                temp /= 10;
-            }
-            if(s.find(n) != s.end()) break;
-            s.insert(n);
+        int slow = n, fast = nextNum(n);
+        while(fast != 1 && slow != fast) {
+            slow = nextNum(slow);
+            fast = nextNum(nextNum(fast));
         }   
-        return n == 1;
+        return fast == 1;
     }
 };
