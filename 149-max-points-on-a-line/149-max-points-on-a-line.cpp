f6b57cfa9a5ba158dfe0128ab0b047a9f624dc63
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-21 22:33:36 +0530

    Time: 23 ms (39.49%), Space: 8.1 MB (85.74%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -4,7 +4,7 @@ public:
         int size = points.size();
         unordered_map<float, int> slopes;
         int max = 0;
-        for(int i = 0; i < size - 1; i++) {
+        for(int i = 0; i < size - max- 1; i++) {
             for(int j = i + 1; j < size; j++) {
                 float y = (points[j][1] - points[i][1]), x = (points[j][0] - points[i][0]);
                 float slope;
