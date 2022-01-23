b90f027bec7211e38f2661f2486e405543bb28ff
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-23 19:23:29 +0530

    Time: 6 ms (51.05%), Space: 7.9 MB (73.43%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    int helper(int num) {
-        int count = 0;
-        while(num > 0) {
-            num &= (num - 1);
-            count++;
-        }
-        return count;
-    }
+//     int helper(int num) {
+//         int count = 0;
+//         while(num > 0) {
+//             num &= (num - 1);
+//             count++;
+//         }
+//         return count;
+//     }
+    
+//     vector<int> countBits(int n) {
+//         vector<int> ans(n + 1, 0);
+//         for(int i = 0; i <= n; i++) {
+//             ans[i] = helper(i);
+//         }
+//         return ans;
+//     }
+    
     
     vector<int> countBits(int n) {
         vector<int> ans(n + 1, 0);
-        for(int i = 0; i <= n; i++) {
-            ans[i] = helper(i);
+        int offSet = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i > 0 && ((i & (i - 1)) == 0)) {
+                offSet = i;
+                ans[i] = 1;
+            } else {
+                ans[i] = 1 + ans[i - offSet];            
+            }
         }
         return ans;
     }
