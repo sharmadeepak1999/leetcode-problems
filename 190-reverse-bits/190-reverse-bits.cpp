08d9e1ec7a24abdba9c719f850d3efd8fc9365cb
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-23 18:39:47 +0530

    Time: 8 ms (6.76%), Space: 5.9 MB (50.71%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     
 uint32_t reverseBits(uint32_t n) {
-    uint32_t temp = n;
-    for(int j = 0; j < 32; j++) {
-        if(temp & 1) {
-            n |= (1 << (31 - j));
-        } else {
-            n &= ~(1 << (31 - j));
-        }
-        temp >>= 1;
-    }
-    return n;
+    // uint32_t temp = n;
+    // for(int j = 0; j < 32; j++) {
+    //     if(temp & 1) {
+    //         n |= (1 << (31 - j));
+    //     } else {
+    //         n &= ~(1 << (31 - j));
+    //     }
+    //     temp >>= 1;
+    // }
+    // return n;
+    
+    uint32_t ans = 0;
+    for(int i = 0; i < 32; i++) ans = (ans << 1) | ((n >> i) & 1);
+    return ans;
 }
 
 };
