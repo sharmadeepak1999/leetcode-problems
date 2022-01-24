9e532e0b61ba6759364190b4914badd121941228
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-24 18:48:39 +0530

    Time: 2 ms (21.74%), Space: 5.9 MB (38.57%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-       int nums[n];
         int i = 0, XOR = 0;
-        nums[i++] = start + 2 * i;
         while(i < n) {
-            nums[i] = start + 2 * i;
-            XOR ^= nums[i - 1];
+            XOR ^= start + 2 * i;
             i++;
         }
-        XOR ^= nums[i - 1];
         return XOR;
     }
 };
