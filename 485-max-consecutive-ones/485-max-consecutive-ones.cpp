95e447a6ff486084b6cf38797a4297cd2251dfb8
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-27 22:24:22 +0530

    Time: 65 ms (22.52%), Space: 36.2 MB (20.82%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -4,12 +4,8 @@ public:
         int maxConOne = 0, currMax = 0;
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] == 1) {
-                if(currMax == 0) {
-                    currMax = 1;
-                } else {
-                    currMax++;
-                }
-                if(currMax > maxConOne) maxConOne = currMax;
+                currMax++;
+                maxConOne = max(maxConOne, currMax);
             } else {
                 currMax = 0;
             }
