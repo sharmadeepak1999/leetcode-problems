1a967d5d87c3f94e2707122ad66f64a1ef1bcebb
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-28 20:12:09 +0530

    Time: 19 ms (69.82%), Space: 19.7 MB (50.45%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // int n = nums.size(), i;
-        // for(i = 0; i < n; i++) {
-        //     int count = 1;
-        //     for(int j = i + 1; j < n; j++) {
-        //         if(nums[j] == nums[i]) count++;
-        //     }
-        //     if(count > (n / 2)) break;
-        // }
-        // return nums[i];
-
+//         int n = nums.size(), i;
+//         for(i = 0; i < n; i++) {
+//             int count = 1;
+//             for(int j = i + 1; j < n; j++) {
+//                 if(nums[j] == nums[i]) count++;
+//             }
+//             if(count > (n / 2)) break;
+//         }
+//         return nums[i];
+// ==================================================================
         
-        int n = nums.size();
-        unordered_map<int, int> freq;
-        for(int i = 0; i < n; i++) freq[nums[i]]++;
+//         int n = nums.size();
+//         unordered_map<int, int> freq;
+//         for(int i = 0; i < n; i++) freq[nums[i]]++;
         
-        unordered_map<int, int>::iterator it = freq.begin();
-        while(it != freq.end()) {
-            if(it -> second > (n / 2)) break;
-            it++;
+//         unordered_map<int, int>::iterator it = freq.begin();
+//         while(it != freq.end()) {
+//             if(it -> second > (n / 2)) break;
+//             it++;
+//         }
+//         return it -> first;
+//         ===========================================================
+        int count = 1, me = 0, n = nums.size();
+        for(int i = 1; i < n; i++) {
+            if(nums[i] == nums[me]) {
+                count++;
+            } else count--;
+            if(!count) {
+                count = 1;
+                me = i;
+            }
         }
-        return it -> first;
+        
+        count = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[me] == nums[i]) count++;
+            
+            if(count > n / 2) return nums[me];
+        }
+        return -1;
     }
 };
