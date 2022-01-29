a6e30f0ca18aec7435ebafe5792ec0b43d31d139
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-01-29 20:00:12 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (49.48%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int size = nums.size();
-        if(size == 1) return 0;
-        if(nums[0] > nums[1]) return 0;
-        if(nums[size - 1] > nums[size - 2]) return size - 1;
+//         int size = nums.size();
+//         if(size == 1) return 0;
+//         if(nums[0] > nums[1]) return 0;
+//         if(nums[size - 1] > nums[size - 2]) return size - 1;
         
-        for(int i = 1; i < size - 1; i++)
-            if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+//         for(int i = 1; i < size - 1; i++)
+//             if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+//         return -1;
+        
+        int low = 0, high = nums.size() - 1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if((mid == 0 || nums[mid - 1] <= nums[mid]) && (mid == nums.size() - 1 || nums[mid] >= nums[mid + 1])){
+                return mid;
+            }
+            if(mid > 0 && nums[mid - 1] >= nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
         return -1;
     }
 };
