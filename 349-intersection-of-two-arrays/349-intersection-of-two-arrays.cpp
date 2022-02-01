7c5293bcf2d7543c9d9b9e1e00ebf4822a272398
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-02-01 18:03:36 +0530

    Time: 6 ms (64.53%), Space: 9.9 MB (91.34%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> hashmap;
-        vector<int> ans;
-        for(auto x : nums1) {
-            if(hashmap.find(x) == hashmap.end()) {
-                hashmap[x] = 1;
-            }
-        }
+//         unordered_map<int, int> hashmap;
+//         vector<int> ans;
+//         for(auto x : nums1) {
+//             if(hashmap.find(x) == hashmap.end()) {
+//                 hashmap[x] = 1;
+//             }
+//         }
         
-        for(auto x : nums2) {
-            if(hashmap.find(x) != hashmap.end()) {
-                ans.push_back(x);
-                hashmap.erase(x);
+//         for(auto x : nums2) {
+//             if(hashmap.find(x) != hashmap.end()) {
+//                 ans.push_back(x);
+//                 hashmap.erase(x);
+//             }
+//         }
+//         return ans;
+        
+        vector<int> ans;
+        int size1 = nums1.size(), prev = 0;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        for(int i = 0; i < size1; i++) {
+            if(i == 0 || nums1[i] != prev) {
+                if(binary_search(nums2.begin(), nums2.end(), nums1[i])) {
+                    ans.push_back(nums1[i]);
+                }   
             }
+            prev = nums1[i];
         }
         return ans;
     }
