77d2c77dcb44594714d8d117d7a27649d4aa4350
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-05-10 21:14:58 +0530

    Time: 29 ms (61.68%), Space: 19.2 MB (83.81%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+    /*
+        // traverse from begin, and if non zero then update the value of the pointer maintained with current value and increment the pointer, and if zero then count increment. after first loop insert zero in the remaining part of the array.
         int i, j;
         i = j = 0;
         int z = 0;
@@ -14,6 +16,19 @@ public:
         while(z) {
             nums[nums.size() - z] = 0;
             z--;
+        }
+    */
+        
+        // traverse from begin, and if non zero then swap from the other pointer maintained and increment that pointer. zero will be taken care by itself using this algo.
+        int i, j;
+        i = j = 0;
+        int z = 0;
+        while(i < nums.size()) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+            i++;
         }
     }
 };
