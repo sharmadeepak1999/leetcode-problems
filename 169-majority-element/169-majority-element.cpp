dedf4b418d170e8bb78dffd4d4ad8f311380e065
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-01 21:27:05 +0530

    Time: 25 ms (58.81%), Space: 19.6 MB (59.70%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,44 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-//         int n = nums.size(), i;
-//         for(i = 0; i < n; i++) {
-//             int count = 1;
-//             for(int j = i + 1; j < n; j++) {
-//                 if(nums[j] == nums[i]) count++;
-//             }
-//             if(count > (n / 2)) break;
-//         }
-//         return nums[i];
-// ==================================================================
+        // we can use a hashmap and store the frequency of each element, and during storing if the element frequency goes above n/2, then return that element.
         
-//         int n = nums.size();
-//         unordered_map<int, int> freq;
-//         for(int i = 0; i < n; i++) freq[nums[i]]++;
+        unordered_map<int, int> m;
         
-//         unordered_map<int, int>::iterator it = freq.begin();
-//         while(it != freq.end()) {
-//             if(it -> second > (n / 2)) break;
-//             it++;
-//         }
-//         return it -> first;
-//         ===========================================================
-        int count = 1, me = 0, n = nums.size();
-        for(int i = 1; i < n; i++) {
-            if(nums[i] == nums[me]) {
-                count++;
-            } else count--;
-            if(!count) {
-                count = 1;
-                me = i;
-            }
-        }
-        
-        count = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[me] == nums[i]) count++;
-            
-            if(count > n / 2) return nums[me];
+        int n = nums.size();
+        for(auto a:nums) {
+            m[a]++;
+            if(m[a] > (n / 2)) return a;
         }
         return -1;
     }
