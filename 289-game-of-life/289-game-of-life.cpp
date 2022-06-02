4158cdc3b50cb5ff5abbc85c53052348a16c7769
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-02 22:14:23 +0530

    Time: 0 ms (100.00%), Space: 7 MB (61.13%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>> newBoard = board;
+        
+        // Brute force, we will visit each element in the matrix or board, one by one, and count the alive neighbours while making sure to be in the boundary of matrix, after that we will update the state of current element according to the given constraints.
+        
         int m = board.size(), n = board[0].size();
         
         for(int i = 0; i < m; i++) {
@@ -9,30 +11,36 @@ public:
                 int nalive = 0;
                 
                 if(j < n - 1) {
-                    if(board[i][j + 1] == 1) nalive++;
+                    if(board[i][j + 1] == 1 || board[i][j + 1] == -1) nalive++;
                     
-                    if(i >= 1 && board[i - 1][j + 1] == 1) nalive++;
+                    if(i >= 1 && (board[i - 1][j + 1] == 1 || board[i - 1][j + 1] == -1)) nalive++;
                     
-                    if(i < m - 1 && board[i + 1][j + 1] == 1) nalive++;
+                    if(i < m - 1 && (board[i + 1][j + 1] == 1 || board[i + 1][j + 1] == -1)) nalive++;
                 }
                 
                 if(j >= 1) {
-                    if(board[i][j - 1] == 1) nalive++;
+                    if(board[i][j - 1] == 1 || board[i][j - 1] == -1) nalive++;
                     
-                    if(i < m - 1 && board[i + 1][j - 1] == 1) nalive++;
-                    if(i >= 1 && board[i - 1][j - 1] == 1) nalive++;
+                    if(i < m - 1 && (board[i + 1][j - 1] == 1 || board[i + 1][j - 1] == -1)) nalive++;
+                    if(i >= 1 && (board[i - 1][j - 1] == 1 || board[i - 1][j - 1] == -1)) nalive++;
                     
                 }
                 
-                if(i < m - 1 && board[i + 1][j] == 1) nalive++;
-                if(i >= 1 && board[i - 1][j] == 1) nalive++;
+                if(i < m - 1 && (board[i + 1][j] == 1 || board[i + 1][j] == -1)) nalive++;
+                if(i >= 1 && (board[i - 1][j] == 1 || board[i - 1][j] == -1)) nalive++;
                 
-                if(board[i][j] == 1) {
-                    if(nalive < 2 || nalive > 3) newBoard[i][j] = 0;
-                } else if(nalive == 3) newBoard[i][j] = 1;
-                
+                if(board[i][j] == 1 || board[i][j] == -1) {
+                    if(nalive < 2 || nalive > 3) board[i][j] = -1;
+                } else if(nalive == 3) board[i][j] = -2;
             } 
         }
-        board = newBoard;
+        
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == -1) board[i][j] = 0;
+                else if(board[i][j] == -2) board[i][j] = 1;
+            } 
+        }
     }
 };
