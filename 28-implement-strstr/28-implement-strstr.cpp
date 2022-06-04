af139efb6a3256eeb9bb8486bd99564b2d2f07e0
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-04 20:27:24 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (90.32%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-//         if(needle == "") return 0;
-//        for(int i = 0; i <= haystack.length() - needle.length(); i++){
-//                 int j = 0;
-//                for(; j < needle.length(); j++) {
-//                    if(haystack[i + j] != needle[j]) break;
-//                }
-               
-//                if(j == needle.length()) return i;
-//        } 
-//         return -1;
+        
+        // We could use the brute force approach to check all poisbility, we start from the first char in the array, if it is equal to the first char in the needle, then we keep continuing, if ever it becomes unequal we break the loop, after that we check if the counter of the inner loop has become equal to the lenght of needle string that shows that all its char were found in the haystack, so return the outer loop index, which shows the starting postion.
+        
         int m = haystack.size(), n = needle.size();
         for (int i = 0; i <= m - n; i++) {
             int j = 0;
