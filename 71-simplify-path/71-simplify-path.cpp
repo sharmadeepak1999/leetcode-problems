a6e84bdb0bbe8c301e53f54640134782db05a2a4
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-05 20:33:27 +0530

    Time: 27 ms (5.86%), Space: 8.2 MB (75.54%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> v;
+        int i = 0;
+        
+        while(i < path.length()) {
+            if(path[i] == '/') {
+                while(i < path.length() && path[i] == '/') {
+                    i++;
+                }
+                v.push_back("/");
+                cout << endl << "/";
+            } else {
+                string op = "";
+                
+                while(i < path.length() && path[i] != '/') {
+                    op += path[i];
+                    i++;
+                }
+                if(op == "..") {
+                    v.pop_back();
+                    if(v.size() >= 2) {
+                        v.pop_back();
+                        v.pop_back();   
+                    }
+                } else if(op == ".") v.pop_back();
+                else v.push_back(op);
+                cout << endl << op;
+            }
+        }
+        
+        string cp = "";
+        if(v.size() > 2 && v.back() == "/") v.pop_back();
+        if(v.size() == 0) cp = "/";
+        for(auto s : v) cp += s;
+        return cp;
+    }
+};
