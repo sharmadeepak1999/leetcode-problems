1d5bb922e85dd40fac39c9a983a8f53ec9439016
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-08 06:55:10 +0530

    Time: 11 ms (69.46%), Space: 7.8 MB (87.48%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int calculate(string s) {
+        /*
+        // We will create a stack of numbers, and traverse through the string, if the current char is a space, then we skip, if it is a digit, we add it to the current number variable, this is to account for numbers with multiple digits, if it is a operation, then we check if it is + or -, if yes then we just insert them into the stack with the appropraiate sign, if they are * or / then we pop the top of stack, and compute the result with the curr number, and push result in to stack, after this we initialize currnum as 0. for the 1st operand we will keep the operation variable default value as +, after every other encounter of a operator, we will update the operation variable. after the loop, we will pop all items in the stack and add them to a result, and return.
+        
         stack<int> nums;
         int currNum = 0;
         char operation = '+';
@@ -13,12 +16,10 @@ public:
                     else if(operation == '*') {
                         int num = nums.top();
                         nums.pop();
-                        cout << num << '*' << currNum << '=' << num * currNum << endl;
                         nums.push(num * currNum);
                     } else if(operation == '/') {
                         int num = nums.top();
                         nums.pop();
-                                cout << num << '/' << currNum << '=' << num / currNum << endl;
                         nums.push(num / currNum);
                     }
                     currNum = 0;
@@ -28,10 +29,39 @@ public:
         
         int res = 0;
         while(!nums.empty()) {
-            cout << '(' << nums.top() << ") " << "+ ";
             res += nums.top();
             nums.pop();
         }
+        return res;
+        
+        */
+        
+        int lastNum = 0;
+        int res = 0; 
+        int currNum = 0;
+        char operation = '+';
+        for(int i = 0; i < s.length(); i++) {
+            char c = s[i];
+                if(isdigit(c)) currNum = currNum * 10 + (c - '0');
+                if(!isdigit(c) && !iswspace(c) || (i == s.length() - 1)){
+                    if(operation == '+') {
+                        res += lastNum;
+                        lastNum = currNum;
+                    }
+                    else if(operation == '-'){
+                        res += lastNum;
+                        lastNum = -currNum;
+                    }
+                    else if(operation == '*') {
+                        lastNum *= currNum;
+                    } else if(operation == '/') {
+                        lastNum /= currNum;
+                    }
+                    currNum = 0;
+                    operation = c;
+                }
+        }
+        res += lastNum;
         return res;
     }
 };
