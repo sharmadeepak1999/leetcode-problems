e8d5411335eecceeb2e4345ed9ca559995f1749f
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-16 16:18:05 +0530

    Time: 3 ms (60.17%), Space: 8.3 MB (18.15%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -10,6 +10,11 @@
  */
 class Solution {
 public:
+    /*
+    
+    // We will use recursion, we will traverse the linked list using recursion, we call the recursion for the next node, before processing the current node, this is to process the last node first, from the last node we will construct the number, using references of num, and the index or power of i or the position of each digits in the binary number.
+    
+    
     void helper(ListNode *head, int &num, int &i) {
         if(!head) return;
         
@@ -22,6 +27,23 @@ public:
     int getDecimalValue(ListNode* head) {
         int num = 0, i = 0;
         helper(head, num, i);
+        return num;
+    }
+    */
+    
+    
+    int getDecimalValue(ListNode* head) {
+        // Another method could be find the length of the linked list, and then linearly traverse and compute the number by decrementing the found length, len - 1 for first node, len - 2 for the second node, so on. 
+        
+        // The efficient approach that would require only one pass, would be to assume that there is only one node in the ll, so we calculate the num, assuming single node, i.e, 2^0, now if we find another node then we have to multiply the num by 2, to increase the power of 2 for the previous position, so on we will calculate.
+        
+        int num = 0;
+        
+        while(head) {
+            num = num * 2; // Increasing the power of two for all previous node
+            num += (head -> val) * 1; // Assuming this is the last node so 2 ^ 0 = 1
+            head = head -> next;
+        }
         return num;
     }
 };
