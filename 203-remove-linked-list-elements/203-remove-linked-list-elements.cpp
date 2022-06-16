eb3d9e7f886db004b76bc9c36f06954a22bd7c64
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-16 17:39:57 +0530

    Time: 44 ms (20.68%), Space: 15.2 MB (18.76%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -11,14 +11,41 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
+        /*
+        
+        // We can use recursion, we can recursion for the rest ll, and check if the head val is equal to the val to remove, if yes then only return the new head from the recursion, and delete the head, if not equal the link the new head from the curr head next, and return curr head.
+        
         if(head == NULL) return NULL;
         
         ListNode* newHead = removeElements(head -> next, val);
         
-        if(head -> val == val) return newHead;
+        if(head -> val == val) {
+            delete head;
+            return newHead;
+        }
         else {
             head -> next = newHead;
             return head;
         }
+        */
+        
+        ListNode *temp = head; 
+        
+        while(temp && temp -> next) {
+            if(temp -> next -> val == val) {
+                ListNode *t = temp -> next;
+                temp -> next = temp -> next -> next;
+                delete t;
+            } else {
+                temp = temp -> next;
+            }
+        }
+        
+        if(head && head -> val == val) {
+            ListNode *t = head;
+            head = head -> next;
+            delete t;
+        }
+        return head;
     }
 };
