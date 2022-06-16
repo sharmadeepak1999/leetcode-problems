81eb692fe1d73fd19a80a78dba15158687847667
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-16 15:07:22 +0530

    Time: 11 ms (81.08%), Space: 8.1 MB (57.35%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,6 +9,8 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        /*
+        // Naive method, We will use a hashmap to store all visited nodes, if we find any node address, already stored in the hashmap, this means that we already have traversed this node, so there is a cycle so we return true, else at last we return false.
         unordered_map<ListNode*, bool> m;
         
         ListNode* temp = head;
@@ -19,6 +21,20 @@ public:
             m[temp] = true;
             temp = temp -> next;
         }
+        return false;
+        */
+        
+        
+        // A cool approach, would be to use a slow and fast pointer, the intiution is that if there exists a cycle then, the slow and fast pointer is bound to meet, we move slow by 1 step, and fast by two step. 
+        
+        ListNode *slow = head, *fast = head;
+        
+        while(fast && fast -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(slow == fast) return true;
+        }
+        
         return false;
     }
 };
