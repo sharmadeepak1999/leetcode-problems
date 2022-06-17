9345a9a6af456a14f44499c3633aea71c15aaad4
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-17 17:40:42 +0530

    Time: 381 ms (30.49%), Space: 117.9 MB (61.20%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,7 +10,33 @@
  */
 class Solution {
 public:
+    ListNode* middleLL(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        
+        while(fast -> next && fast -> next -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        return slow;
+    }
+    
+    
+    ListNode* reverseLL(ListNode* head) {
+        ListNode *temp = head, *prev = NULL;
+        
+        while(temp) {
+            ListNode *t = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = t;
+        }
+        return prev;
+    }
+    
     bool isPalindrome(ListNode* head) {
+        /*
+        // Use a vector or array to store all the elements of the linked list, then use the two pointer approach to check if the left element and right element is equal then continue, or return false.
+        
         vector<int> arr;
         
         ListNode *temp = head;
@@ -24,6 +50,20 @@ public:
         
         while(l < r) {
             if(arr[l++] != arr[r--]) return false;
+        }
+        return true;
+        */
+        
+        
+        ListNode *mid = middleLL(head);
+        mid -> next = reverseLL(mid -> next);
+        
+        ListNode *temp = head, *temp2 = mid -> next;
+        
+        while(temp2) {
+            if(temp2 -> val != temp -> val) return false;
+            temp2 = temp2 -> next;
+            temp = temp -> next;
         }
         return true;
     }
