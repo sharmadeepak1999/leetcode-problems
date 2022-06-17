d87d8a228abbeea3969659a13b0ef9a666d2e6f1
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-17 15:02:57 +0530

    Time: 10 ms (63.23%), Space: 14.9 MB (14.54%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,6 +11,8 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        /*
+        // We can use similar logic to merge sort function, but here we need to create a dummy node, to have a generic function. this will use a new external linked list.
         ListNode *newHead = new ListNode();
         ListNode *temp = newHead;
         while(list1 && list2) {
@@ -33,11 +35,33 @@ public:
         }
         
         while(list2) {
-            
             temp -> next = new ListNode();
             temp = temp -> next;
             temp -> val = list2 -> val;
             list2 = list2 -> next;
+        }
+        return newHead -> next;
+        */
+        ListNode* newHead = new ListNode();
+        ListNode* temp = newHead;
+        while(list1 && list2) {
+            if(list1 -> val > list2 -> val) {
+                ListNode *t1 = temp -> next;
+                temp -> next = list2;
+                ListNode *t2 = list2;
+                list2 = list2 -> next;
+                t2 -> next = t1;
+            } else {
+                temp -> next = list1;
+                list1 = list1 -> next;
+            }
+            temp = temp -> next;
+        }
+        if(list1) {
+            temp -> next = list1;
+        } 
+        if(list2) {
+            temp -> next = list2;
         }
         return newHead -> next;
     }
