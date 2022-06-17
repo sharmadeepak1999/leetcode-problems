e6ccc25898b09870806ce408836e7941b563308c
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-17 16:20:44 +0530

    Time: 58 ms (66.40%), Space: 14.5 MB (72.19%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,6 +9,9 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        /*
+        // We can use a hashmap to store the nodes of the first ll, and then traverse the second ll, and check if any of its node is present in the hashmap, if yes then return that node.
+        
         unordered_set<ListNode*> s;
         
         ListNode* temp = headA;
@@ -23,5 +26,34 @@ public:
         while(temp && s.find(temp) == s.end()) temp = temp -> next;
         
         return temp;
+        */
+        
+        int l1 = 0, l2 = 0;
+        
+        ListNode *tempA = headA, *tempB = headB;
+        while(tempA || tempB) {
+            if(tempA) {
+                l1++;
+                tempA = tempA -> next;
+            }
+            if(tempB) {
+                l2++;
+                tempB = tempB -> next;
+            }
+        }
+        
+        tempA = headA;
+        tempB = headB;
+        if(l1 > l2) {
+            for(int i = 1; i <= l1 - l2; i++) tempA = tempA -> next;
+        } else if(l2 > l1) {
+            for(int i = 1; i <= l2 - l1; i++) tempB = tempB -> next;
+        }
+        
+        while(tempA && tempB && tempA != tempB) {
+            tempA = tempA -> next;
+            tempB = tempB -> next;
+        }
+        return tempA;
     }
 };
