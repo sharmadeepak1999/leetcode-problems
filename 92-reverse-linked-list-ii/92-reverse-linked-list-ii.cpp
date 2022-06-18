9df5d8e5626bd41d5fdc40e5d0e1d5964b1480a8
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-18 18:33:35 +0530

    Time: 6 ms (27.61%), Space: 7.5 MB (17.72%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -11,6 +11,9 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+    /*
+        // We can traverse the linked list and store the left and right pointers in seperate variables, also we will store the reference to the left of left pointer, and right of right pointer, now we will reverse the list from left to right pointer as standard algo, after that we will link the reversed list to the main list with the help of the pointers stored of extremes stored.
+        
         ListNode* temp = head;
         
         ListNode *l = NULL, *r = NULL, *ul = NULL, *ur = NULL, *prev = NULL;
@@ -42,6 +45,31 @@ public:
         if(ul) ul -> next = r;
         else head = r;
         l -> next = ur;
+        return head;
+    */
+    
+        ListNode *temp = head, *ul = NULL, *prev = NULL, *l = NULL;
+        int i = 1;
+        while(temp && i <= right) {
+            while(i < left) {
+                ul = temp;
+                temp = temp -> next;
+                i++;
+            }
+            if(i == left) l = temp;
+            while(i >= left && i <= right) {
+                ListNode *t = temp -> next;
+                temp -> next = prev;
+                prev = temp;
+                temp = t;
+                i++;
+            }
+        }
+        
+        if(ul) ul -> next = prev;
+        else head = prev;
+        
+        l -> next = temp;
         return head;
     }
 };
