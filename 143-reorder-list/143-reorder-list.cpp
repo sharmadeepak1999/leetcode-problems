0f27e73190d6c586612dafd73afbbaa2e6ec84b9
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-19 15:06:22 +0530

    Time: 70 ms (33.25%), Space: 17.8 MB (50.71%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -10,8 +10,31 @@
  */
 class Solution {
 public:
+    ListNode* middleLL(ListNode* head) {
+        ListNode* slow = head, *fast = head;
+        
+        while(fast -> next && fast -> next -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        return slow;
+    }
+    
+    ListNode* reverseLL(ListNode* head) {
+        ListNode* temp = head, *prev = NULL;
+        
+        while(temp) {
+            ListNode* t = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = t;
+        }
+        return prev;
+    }
     void reorderList(ListNode* head) {
-        // Store the ll nodes in a vector, and then using left and right pointer create the links also maintain the whole list using another head variable
+    /*
+        // Store the ll nodes in a vector, and then using left and right pointer create the links also maintain the whole list using another head variable that has a dummy node in the beginning. and at last replace the dummy node with with the original head.
+        
         vector<ListNode*> v;
         
         ListNode* temp = head;
@@ -37,5 +60,20 @@ public:
         }
         temp2 -> next = NULL;
         head = newHead -> next;
+    */
+    
+        
+        ListNode* mid = middleLL(head);
+        
+        ListNode* temp = head, *temp2 = reverseLL(mid -> next);
+        mid -> next = NULL;
+        while(temp2){
+            ListNode* t1 = temp -> next;
+            ListNode* t2 = temp2 -> next;
+            temp -> next = temp2;
+            temp2 -> next = t1;
+            temp = t1;
+            temp2 = t2;
+        }
     }
 };
