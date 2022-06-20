89968c8fa0c348c75f75630b1294238d0bea6fb9
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-20 21:21:27 +0530

    Time: 37 ms (25.52%), Space: 17.9 MB (64.12%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        /*
+        // Calculate the the required sum that would we from 0 to n, and then get the given sum from all the elements from the nums array. the difference would be the missing number.
+        
         int n = nums.size();
         int sum = (n * (n + 1)) / 2;
         
         int givenSum = accumulate(nums.begin(), nums.end(), 0);
         return sum - givenSum;
+        */
+        
+        int res = nums.size();
+        
+        for(int i = 0; i < nums.size(); i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
     }
 };
