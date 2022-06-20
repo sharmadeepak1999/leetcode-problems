3f255f07d064f3ce0f504df4cdbdbbef97831f53
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-20 13:16:13 +0530

    Time: 13 ms (45.91%), Space: 11.1 MB (58.18%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -11,6 +11,8 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
+        // We will check if the next node and the next to next node has value same, then we will run a loop and skip all those elements, and once the loop is complete we will make the current next pointer to point to the next of the next node that would contain the node that is not equal to the next node. we will only move the temp node when the we don't find a duplicate for that we will keep a variable, also we will take use of dummy node to avoid checking for edge cases like deleting node from the beginning of the ll.
+        // We can also do this with a previous node method.
         ListNode* dummy = new ListNode();
         dummy -> next = head;
         ListNode* temp = dummy;
