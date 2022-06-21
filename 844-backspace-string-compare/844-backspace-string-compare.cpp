004a3e012d07a9a0c628542697664130d8355765
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-21 18:13:12 +0530

    Time: 7 ms (8.12%), Space: 6.1 MB (87.37%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
+        // Process each string, insert into a new string if it is not #, else delete the last inserted char from the new string variable. do the same for the other string too. at last compare both the new string formed if equal return true. basically we are using the new string variable as a stack.
+        // The other approach would be to use a stack itself instead of string.
+
         string s1 = "";
         
         for(auto &c : s) {
@@ -13,6 +16,6 @@ public:
             if(c == '#' && s2.size()) s2.pop_back();
             else if(c != '#') s2.push_back(c);
         }
-        return s1 == s2;
+        return s1 == s2;        
     }
 };
