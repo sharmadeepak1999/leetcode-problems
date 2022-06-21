58abd1e287544dfc82900058ceb26e6f677aea30
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-21 16:44:00 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (13.05%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,17 +1,15 @@
 class MyQueue {
 public:
+    /*
+    
+    // Insert into one stack during push operation, but while doing pop or peek check if the other stack is empty if yes then put all the elements of first stack into the other stack, and use the other stack for returning the results. this will give amortized complexity.
+    
     stack<int> s1, s2;
     MyQueue() {
     }
     
     void push(int x) {
         s1.push(x);
-        if(s2.empty()) {
-            while(s1.size()) {
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
     }
     
     int pop() {
@@ -38,6 +36,39 @@ public:
     
     bool empty() {
         return s1.empty() && s2.empty();
+    }
+    */
+    
+    
+    // A not so better approach would be to always before push operation, transfer the content of first stack to the other, and then push the element to the first stack then retransfer the content back from the other stack to first stack. this is to maintain the top as the front of the queue. this will make push O1 time c.
+    stack<int> s1, s2;
+    MyQueue() {
+    }
+    
+    void push(int x) {
+        while(!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while(!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+    
+    int pop() {
+        int x = s1.top();
+        s1.pop();
+        return x;
+    }
+    
+    int peek() {
+        return s1.top();
+    }
+    
+    bool empty() {
+        return s1.empty();
     }
 };
 
