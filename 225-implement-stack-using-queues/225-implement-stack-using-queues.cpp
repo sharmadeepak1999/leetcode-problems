0704c4a9aaf58484111a77244315ce49bd63b868
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-22 15:28:56 +0530

    Time: 6 ms (9.80%), Space: 6.7 MB (87.09%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,5 +1,7 @@
 class MyStack {
 public:
+    /*
+    // We can use two queue, when pushing we pop all the element of the first queue to the second queue, and then push the new element into the first queue, and then push all the element from the second queue to the first queue. this will make the push operation always O(n) time complexity. and other operation O1
     queue<int> q1, q2;
     
     MyStack() {
@@ -30,6 +32,38 @@ public:
     
     bool empty() {
         return q1.empty();
+    }
+    */
+    
+    
+    queue<int> q;
+    
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int size = q.size();
+        while(size > 1) {
+            q.push(q.front());
+            q.pop();
+            size--;
+        }
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
     }
 };
 
