441dcec966f95c632b33a2abd59477901faba0fc
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-22 16:17:27 +0530

    Time: 14 ms (33.32%), Space: 8.8 MB (56.83%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        /*
+        // A naive approach is to use three loops, loop through nums1, for every element in it check the match in nums2, when found, start a loop from the next index of the matched index, and check if a greater number exists, if yes then add it to ans array, else add -1. 
         vector<int> ans;
         
         for(auto &a : nums1) {
@@ -17,6 +19,27 @@ public:
                     break;
                 }
             }
+        }
+        return ans;
+        */
+        
+        
+        unordered_map<int, int> m;
+        stack<int> s;
+        s.push(-1);
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+            while(s.top() < nums2[i] && s.top() != -1) {
+                s.pop();
+            }
+            m[nums2[i]] = s.top();
+            
+            s.push(nums2[i]);
+        }
+        
+        vector<int> ans;
+        
+        for(auto &a : nums1) {
+            ans.push_back(m[a]);
         }
         return ans;
     }
