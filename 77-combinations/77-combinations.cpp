d6eaffc1a53cc587a6b10fbfdb3b08c5fcfc998a
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-23 22:55:22 +0530

    Time: 32 ms (68.82%), Space: 9.2 MB (67.66%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -13,6 +13,8 @@ public:
         }
     }
     vector<vector<int>> combine(int n, int k) {
+        // We will fix a number from the range and call recursion on it, we will do this for all the number in the range using a for loop, if the size of the subset becomes equal to the k, then we push the current subset into ans, and return. also after the recursion call returns we pop out the last element from the subset that we have previously added.
+        
         vector<vector<int>> ans;
         vector<int> sub;
         
