b89f720d1a9e6ede4dc4fe35bd3701484be8c550
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-24 14:59:49 +0530

    Time: 1322 ms (5.00%), Space: 56.9 MB (13.58%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -15,6 +15,9 @@ public:
         }
     }
     int countArrangement(int n) {
+        //We can select an element from the range using a for loop and call recursion for the next index if the current element is not present in the set, we will also check the condition of both divisible operation.
+        // One another approaxh would be to create new nums array with all the number within the range from 1-n, now call the recursive call with nums and curr index, now for run a loop in the recur func and swap every element with the first element or the current index, and call the recursion on the next index, this is because we want to generate all permutation starting with each number, we will before inserting will also check for the divisibility condition then only call the recursion else not.
+        
         unordered_set<int> s;
         int count = 0;
         helper(n, 1, count, s);
