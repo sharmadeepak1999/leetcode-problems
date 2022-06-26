976ab2bd186855ead4dfd7a211c6ae09baf10dbb
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-26 17:28:34 +0530

    Time: 3 ms (35.29%), Space: 6.2 MB (14.81%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -21,13 +21,30 @@ public:
     
     */
     
+    
+    
+    /*
     int helper(int n, vector<int> &dp) {
         if(n == 0 || n == 1) return 1;
         if(dp[n] > 0) return dp[n];
         return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
     }
     int climbStairs(int n) {
+        // We can create memoization using dp vector.
         vector<int> dp(n + 1, false);
         return helper(n, dp);
+    }
+    */
+    
+    
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, false);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
