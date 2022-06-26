e1b54909a6943ad499f2007ea38aeada1f9223da
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-26 23:10:13 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (75.26%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,12 +1,51 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, vector<int>& dp, int index) {
-        if(index >= nums.size()) return 0;
-        if(dp[index] != -1) return dp[index];
-        return dp[index] = max(nums[index] + helper(nums, dp, index + 2), helper(nums, dp, index + 1));
+    /*
+    int helper(vector<int>& nums, int i, int n) {
+        if(i > n) return 0;
+        
+        int maxMoney = 0;
+        for(int j = i; j < n; j++) {
+            maxMoney = max(maxMoney, nums[j] + helper(nums, j + 2, n));
+        }
+        return maxMoney; 
+    }
+    int rob(vector<int>& nums) {
+        // We stand at every house, and add the current house money with the result of recursion on the house 2nd to the current one, we will compare it with so far max money, and at last return it.
+        return helper(nums, 0, nums.size());
+    }
+    */
+    
+    /*
+    int helper(vector<int>& nums, int n) {
+        if(n < 0) return 0;
+        if(n == 0) return nums[n];
+        
+        int pick = nums[n] + helper(nums, n - 2);
+        int notpick = helper(nums, n - 1);
+        return max(pick, notpick);
+    }
+    int rob(vector<int>& nums) {
+        // We can use the pick and not pick method, we can start from the end and first pick the last element and call the recursion on the n - 2 th element, and also once don't pick last element and call the recursion on the n - 1 th element. and return the max of the pick and notpick, the base case would if n < 0 then return 0, and if n == 0 return the 0th element. 
+        
+        return helper(nums, nums.size() - 1);
+    }
+    */
+    
+    
+    int helper(vector<int>& nums, vector<int>& dp, int n) {
+        if(n < 0) return 0;
+        if(n == 0) return nums[n];
+
+        if(dp[n] != -1) return dp[n];
+        
+        int pick = nums[n] + helper(nums, dp, n - 2);
+        int donotpick = helper(nums, dp, n - 1);
+        
+        return dp[n]=max(pick, donotpick);
     }
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size(), -1);
-        return helper(nums, dp, 0);
+        return helper(nums, dp, nums.size() - 1);
     }
 };
