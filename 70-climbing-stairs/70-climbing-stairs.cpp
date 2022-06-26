b25b8420d967cf9935987504f3ffa85da9acde1e
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-26 17:21:46 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (14.81%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-//     int helper(int n, vector<int> &dp) {
-//         if(n < 0) return 0;
-//         if(n == 1 || n == 0) {
-//             return 1;
-//         }
-//         if(dp[n] != 0) return dp[n];
-//         return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
-//     }
-//     int climbStairs(int n) {
-//         vector<int> dp(n + 1, 0);
+    /*
+    void helper(int n, int &ways) {
+        if(n < 0) return;
         
-//         return helper(n, dp);
-//     }
-    
-    int climbStairs(int n) {
-        // Starting from end
-        int one = 1, two = 1;
-        for(int i = 0; i < n - 1; i++) {
-            int temp = one;
-            one = one + two;
-            two = temp;
+        if(n == 0) ways++;
+        else {
+            helper(n - 1, ways);
+            helper(n - 2, ways);
         }
-        return one;
+    }
+    int climbStairs(int n) {
+        // We can use recursion, Top to bottom appraoch, we will start from the nth step, here we have two possibility we can come to nth step from n-1 th step or n-2 th step, we call recursion for each of these step, and if we get to n == 0, then we are sure that the path that we took to come down stairs was correct, and we can increment ways counter, and if we get below n which means that we didn't choose the correct step path so we will simply return. 
+        
+        
+        int ways = 0;
+        helper(n, ways);
+        return ways;
+    }
+    
+    */
+    
+    int helper(int n, vector<int> &dp) {
+        if(n == 0 || n == 1) return 1;
+        if(dp[n] > 0) return dp[n];
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, false);
+        return helper(n, dp);
     }
 };
