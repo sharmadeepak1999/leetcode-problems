ec3a94feadfe08d31d497fb57e3ce4f8eae24f8c
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-28 02:29:36 +0530

    Time: 3 ms (97.88%), Space: 9 MB (21.82%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int helper(vector<vector<int>>& triangle, vector<vector<int>>& dp, int m, int n) {
         if(m == triangle.size() - 1) {
             return triangle[m][n];
@@ -10,7 +11,22 @@ public:
         return dp[m][n] = triangle[m][n] + min(helper(triangle, dp, m + 1, n), helper(triangle, dp, m + 1, n + 1));
     }
     int minimumTotal(vector<vector<int>>& triangle) {
+        // We will use the bottom up approach, we will start from the beginning and will call recursion on the curr index, and curr index + 1 on the next row, the min of both the recursion call will be added to the curr element value, that would be the min path sum for the triangle, base case would be when the last row is reached. memoization is done to optimize runtime.
         vector<vector<int>> dp(triangle.size(), vector<int>(triangle[triangle.size() - 1].size(), INT_MAX));
         return helper(triangle, dp, 0, 0);
+    }
+    */
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        // We will use tabulation now
+        vector<vector<int>> dp(triangle.size() + 1, vector<int>(triangle[triangle.size() - 1].size() + 1, 0));
+        
+        for(int i = triangle.size() - 1; i >= 0; i--) {
+            int n = triangle[i].size();
+            for(int j = n - 1; j >= 0; j--) {
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+        return dp[0][0];
     }
 };
