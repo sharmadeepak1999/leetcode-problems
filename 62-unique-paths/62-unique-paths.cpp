599c244426e6ce685c58eb77a2f42127b165d8f0
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-27 22:20:18 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.35%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -35,7 +35,8 @@ public:
         return helper(m - 1, n - 1, dp);
     }
     */
-    
+
+    /*
     int uniquePaths(int m, int n) {
         // Tabulation method, bottom Up approach. 
         vector<vector<int>> dp(m, vector<int>(n, 0));
@@ -53,4 +54,45 @@ public:
         }
         return dp[m - 1][n - 1];
     }
+    */
+    
+    /*
+    int uniquePaths(int m, int n) {
+        // Tabulation method, bottom Up approach. 
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    */
+    
+    int uniquePaths(int m, int n) {
+        // Tabulation another method, bottom Up approach. 
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) {
+                    dp[0][0] = 1;
+                    continue;
+                }
+                int up = 0, down = 0;
+                if(i >= 1) up = dp[i - 1][j];
+                if(j >= 1) down = dp[i][j - 1];
+                
+                dp[i][j] = up + down;
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    
 };
