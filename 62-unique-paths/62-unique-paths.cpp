b6adac6b2d51a357cf316894823191d6e21c933c
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-27 22:01:14 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.35%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -18,7 +18,7 @@ public:
     }
     */
     
-    
+    /*
     int helper(int m, int n, vector<vector<int>>& dp) {
         if(m == 0 && n == 0) {
             return 1;
@@ -33,5 +33,24 @@ public:
         // Using the method of recursion for counting, return 0 or 1 depending on the base case. we will also use a dp to store overlapping sub problems value. this is top down approach.
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         return helper(m - 1, n - 1, dp);
+    }
+    */
+    
+    int uniquePaths(int m, int n) {
+        // Tabulation method, bottom Up approach. 
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
