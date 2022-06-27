887e9de158d6b18adacea0241557c56511717207
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-27 21:18:25 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.35%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int helper(int i, int j, int m, int n, vector<vector<int>>& dp) {
         if(i == m - 1 && j == n - 1) {
             return 1;
@@ -11,7 +12,26 @@ public:
         return dp[i][j] = helper(i + 1, j, m, n, dp) + helper(i, j + 1, m, n, dp);
     }
     int uniquePaths(int m, int n) {
+        // Using the method of recursion for counting, return 0 or 1 depending on the base case. we will also use a dp to store overlapping sub problems value. this is the bottom approach. 
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         return helper(0, 0, m, n, dp);
+    }
+    */
+    
+    
+    int helper(int m, int n, vector<vector<int>>& dp) {
+        if(m == 0 && n == 0) {
+            return 1;
+        }
+        
+        if(m < 0 || n < 0) return 0;
+        
+        if(dp[m][n] != -1) return dp[m][n];
+        return dp[m][n] = helper(m - 1, n, dp) + helper(m, n - 1, dp);
+    }
+    int uniquePaths(int m, int n) {
+        // Using the method of recursion for counting, return 0 or 1 depending on the base case. we will also use a dp to store overlapping sub problems value. this is top down approach.
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return helper(m - 1, n - 1, dp);
     }
 };
