c3afda9cc61806aac7a5dd6852cc93b8e23224d1
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-28 00:24:20 +0530

    Time: 11 ms (77.27%), Space: 10.1 MB (28.20%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -12,6 +12,8 @@ public:
     }
     */
     
+    
+    /*
     int helper(vector<vector<int>>& grid, vector<vector<int>> &dp, int m, int n) {
         if(m == 0 && n == 0) return grid[0][0];
         if(m < 0 || n < 0) return INT_MAX;
@@ -25,4 +27,29 @@ public:
         vector<vector<int>> dp(m, vector<int>(n, -1));
         return helper(grid, dp, grid.size() - 1, grid[0].size() - 1);
     }
+    */
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        // We will use the tabulation process.
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) dp[0][0] = grid[0][0];
+                else {
+                    int up = INT_MAX;
+                    int left = INT_MAX;
+                    
+                    if(i > 0) up = dp[i - 1][j];
+                    if(j > 0) left = dp[i][j - 1];
+                    
+                    dp[i][j] = grid[i][j] + min(up, left);
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    
 };
