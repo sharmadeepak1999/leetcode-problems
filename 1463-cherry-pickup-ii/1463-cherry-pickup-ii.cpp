4b63c77f2f3b46933192a46824a31a1da6a87cd7
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-28 23:54:39 +0530

    Time: 259 ms (12.66%), Space: 14.4 MB (20.95%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -34,6 +34,40 @@ public:
     }
     */
     
+    /*
+    int helper(vector<vector<int>>& grid, vector<vector<vector<int>>>& dp, int m, int n, int i, int j1, int j2) {
+        if(j1 < 0 || j1 >= n || j2 < 0 || j2 >= n) {
+            return -1e8;
+        }
+        if(i == m - 1) {
+            if(j1 == j2) return grid[i][j1];
+            else return grid[i][j1] + grid[i][j2];
+        }
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxCherry = -1e8;
+        for(int d1 = -1; d1 <= 1; d1++) {
+            for(int d2 = -1; d2 <= 1; d2++) {
+                int currCherry = 0;
+                if(j1 == j2) {
+                    currCherry = helper(grid, dp, m, n, i + 1, j1 + d1, j2 + d2);
+                    maxCherry = max(maxCherry, grid[i][j1] + currCherry);
+                }
+                else {
+                    currCherry = helper(grid, dp, m, n, i + 1, j1 + d1, j2 + d2);
+                    maxCherry = max(maxCherry, grid[i][j1] + grid[i][j2]  + currCherry);
+                }
+            }
+        }
+        return dp[i][j1][j2] = maxCherry; 
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        // We will use memoization using 3d DP.
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
+        return helper(grid, dp, m, n, 0, 0, n - 1);
+    }
+    */
     
     int helper(vector<vector<int>>& grid, vector<vector<vector<int>>>& dp, int m, int n, int i, int j1, int j2) {
         if(j1 < 0 || j1 >= n || j2 < 0 || j2 >= n) {
@@ -61,9 +95,44 @@ public:
         return dp[i][j1][j2] = maxCherry; 
     }
     int cherryPickup(vector<vector<int>>& grid) {
+        // We will use tabulation method.
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
-        return helper(grid, dp, m, n, 0, 0, n - 1);
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
+        
+        for(int j1 = 0; j1 < n; j1++) {
+            for(int j2 = 0; j2 < n; j2++) {
+                if(j1 == j2) dp[m - 1][j1][j2] = grid[m - 1][j1];
+                else dp[m - 1][j1][j2] = grid[m - 1][j1] + grid[m - 1][j2];
+            }
+        }
+        
+        for(int i = m - 2; i >= 0; i--) {
+            for(int j1 = 0; j1 < n; j1++) {
+                for(int j2 = 0; j2 < n; j2++) {
+                    int maxCherry = -1e8;
+                    for(int d1 = -1; d1 <= 1; d1++) {
+                        for(int d2 = -1; d2 <= 1; d2++) {
+                            int currCherry = 0;
+                            if(j1 == j2) {
+                               currCherry = grid[i][j1];
+                            }
+                            else {
+                                currCherry = grid[i][j1] + grid[i][j2];
+                            }
+                            if ((j1 + d1 < 0 || j1 + d1 >= n) ||
+                              (j2 + d2 < 0 || j2 + d2 >= n))
+
+                              currCherry += -1e9;
+                            else
+                              currCherry += dp[i + 1][j1 + d1][j2 + d2];
+                            maxCherry = max(maxCherry, currCherry);
+                        }
+                    }
+                    dp[i][j1][j2] = maxCherry;
+                }
+            }    
+        }
+        return dp[0][0][n - 1];
     }
 };
