6c366217a3c795b083522f48b38fda9bb54d4c72
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-30 03:30:54 +0530

    Time: 49 ms (39.52%), Space: 18.4 MB (40.92%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -20,7 +20,7 @@ public:
     }
     */
     
-    
+    /*
     int helper(vector<int>& coins, vector<vector<int>> &dp, int i, int amount) {
         if(i == 0) {
             return amount % coins[0] == 0;
@@ -39,5 +39,48 @@ public:
         // use memoization to optimize the time.
         vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
         return helper(coins, dp, coins.size() - 1, amount);
+    }
+    */
+    
+     
+    int helper(vector<int>& coins, vector<vector<int>> &dp, int i, int amount) {
+        if(i == 0) {
+            return amount % coins[0] == 0;
+        }
+        if(amount == 0) return 1;
+        
+        if(dp[i][amount] != -1) return dp[i][amount];
+        int nottake = helper(coins, dp, i - 1, amount);
+        int take = 0;
+        if(coins[i] <= amount) {
+            take = helper(coins, dp, i, amount - coins[i]);
+        }
+        return dp[i][amount] = take + nottake;
+    }
+    int change(int amount, vector<int>& coins) {
+        // use tabulation.
+        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, 0));
+        
+        for(int amt = 0; amt <= amount; amt++) {
+            if(amt % coins[0] == 0) {
+                dp[0][amt] = 1;
+            }    
+        }
+        
+        for(int i = 0; i < coins.size(); i++) {
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i < coins.size(); i++) {
+            for(int amt = 0; amt <= amount; amt++) {
+                int nottake = dp[i - 1][amt];
+                int take = 0;
+                if(coins[i] <= amt) {
+                    take = dp[i][amt - coins[i]];
+                }
+                dp[i][amt] = take + nottake;
+            }
+        }
+        return dp[coins.size() - 1][amount];
     }
 };
