7e1b2e0600113afb03bf1070d4ddee0a4fd6dc8b
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-30 02:18:21 +0530

    Time: 375 ms (30.42%), Space: 9 MB (78.08%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int helper(vector<int>& nums, int i, int target){
+        if(i == 0) {
+            if(target == 0 && nums[0] == 0) return 2;
+            if(target == 0 || nums[0] == target) return 1;
+            return 0;
+        }
+        
+        int nottake = helper(nums, i - 1, target);
+        int take = 0;
+        if(nums[i] <= target) {
+            take = helper(nums, i - 1, target - nums[i]);
+        }
+        return take + nottake;
+    } 
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // This problem can be reduced to the problem to divide the array into two subset whose different is target. s1 - s2 = target, (total - s2) - s2 = target, s2 = (total - target) / 2, so we just need to find the subset whose sum is s2.
+        
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if(total - target < 0 || (total - target) % 2) return 0;
+        return helper(nums, nums.size() - 1, (total - target) / 2);
+    }
+};
