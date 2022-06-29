fef1c2e612538357fe6b344b0e95e46b8d306209
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-06-30 03:31:23 +0530

    Time: 54 ms (35.30%), Space: 18.3 MB (47.34%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -42,21 +42,7 @@ public:
     }
     */
     
-     
-    int helper(vector<int>& coins, vector<vector<int>> &dp, int i, int amount) {
-        if(i == 0) {
-            return amount % coins[0] == 0;
-        }
-        if(amount == 0) return 1;
-        
-        if(dp[i][amount] != -1) return dp[i][amount];
-        int nottake = helper(coins, dp, i - 1, amount);
-        int take = 0;
-        if(coins[i] <= amount) {
-            take = helper(coins, dp, i, amount - coins[i]);
-        }
-        return dp[i][amount] = take + nottake;
-    }
+    
     int change(int amount, vector<int>& coins) {
         // use tabulation.
         vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, 0));
