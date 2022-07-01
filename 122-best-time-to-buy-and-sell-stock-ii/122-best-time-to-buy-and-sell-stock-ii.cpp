8f0b9646590efc5b567b00ca7f70d07a628acc6b
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-02 00:58:30 +0530

    Time: 22 ms (10.17%), Space: 15.3 MB (10.70%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -34,7 +34,7 @@ public:
     }
     */
     
-    
+    /*
     int helper(vector<int>& prices, vector<vector<int>> &dp, int i, int buy) {
         if(i == prices.size()) return 0;
     
@@ -53,7 +53,34 @@ public:
     }
     
     int maxProfit(vector<int>& prices) {
+        // Using memoization
         vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
         return helper(prices, dp, 0, 1);
+    }
+    */
+    
+
+    
+    int maxProfit(vector<int>& prices) {
+        // Using tabulation
+        int n = prices.size();
+        
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int i = n - 1; i >= 0; i--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if(buy) {
+                    // Buy at the current price, money will go out of the pocket, so it will reduced form the profit, or we can choose to not   buy, then we will just call recursion for next index, take max of these.
+                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+                } else {
+                    // Sell or not sell depends, add current prices to profit if decides to sell.
+                    profit = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
