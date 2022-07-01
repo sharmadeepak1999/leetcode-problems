a1757e103f75181b5f2d72a529c83dd8934e72e3
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-02 00:17:45 +0530

    Time: 128 ms (97.80%), Space: 93.3 MB (89.36%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
+    /*
     int maxProfit(vector<int>& prices) {
+        // check for every pair in the array using two loops and maintain a max variable, check if the curr pair is max.
+        
+        // Start from the first day price, compare it with second day price if it is greater, then calculate the profit and check if it is max profit, else initialize the buying price as current price.
         int mp = 0;
         int b = prices[0];
         for(auto p : prices) {
@@ -12,5 +16,19 @@ public:
             }
         }
         return mp;
+    }
+    */
+    
+    
+    int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0], profit = 0;
+        
+        int n = prices.size();
+        for(int i = 1; i < n; i++) {
+            int currProfit = prices[i] - minPrice;
+            profit = max(profit, currProfit);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return profit;
     }
 };
