2189d9034229996668f4d4c86323694adc8af646
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-01 22:17:16 +0530

    Time: 145 ms (11.32%), Space: 82.3 MB (5.13%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int helper(string word1, string word2,  vector<vector<int>> &dp, int i, int j) {
+        if(j == 0) return i;
+        if(i == 0) return j;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(word1[i - 1] == word2[j - 1]) {
+            return dp[i][j] = helper(word1, word2, dp, i - 1, j - 1);
+        } else {
+            int del = 1 + helper(word1, word2, dp, i - 1, j);
+            int repl = 1 + helper(word1, word2, dp, i - 1, j - 1);
+            int insert = 1 + helper(word1, word2, dp, i, j - 1);
+            return dp[i][j] = min(del, min(repl, insert));
+        }
+    }
+    int minDistance(string word1, string word2) {
+        // we will start from the last char of both the strings, and check if they are equal, if yes then we don't need to do anything, we will just call the recursion for the next indexes. if they are not equal then we can either delete, insert, or replace charcters, and will accordingly call the recursion, and will take the minimum of all of them and return. the base case would be when either word1 get exhausted, or word2 get exhausted, if the first one get exhausted, then it would mean the minimum number of operations required to convert an empty string that is word1 to word2, which would be basically the current length of word2, j + 1, if the second string get exhausted then we will return the length of the first string, as it would be the min no. char to be deleted to convert word1 to an empty string word2.
+        
+        // We will have to change these to 1 based indexing to help in represeting them in a dp. 
+        
+        // Using memoization
+        
+        int m = word1.length();
+        int n = word2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        
+        return helper(word1, word2, dp, m, n);
+    }
+};
