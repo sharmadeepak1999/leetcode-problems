0e55414825e3ec6a95623f46773f9df5c16b805d
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-03 03:18:45 +0530

    Time: 66 ms (84.66%), Space: 10.3 MB (38.53%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -24,7 +24,7 @@ public:
     }
     */
     
-    
+    /*
     int helper(vector<int>& cuts,  vector<vector<int>> &dp, int i, int j) {
         if(i > j) return 0;
         
@@ -47,5 +47,42 @@ public:
         vector<vector<int>> dp(c + 1, vector<int>(c + 1, -1));
         
         return helper(cuts, dp, 1, c);
+    }
+    */
+    
+    
+    int helper(vector<int>& cuts,  vector<vector<int>> &dp, int i, int j) {
+        if(i > j) return 0;
+        
+        int mini = 1e9;
+        if(dp[i][j] != -1) return dp[i][j];
+        for(int ind = i; ind <= j; ind++) {
+            int cost = cuts[j + 1] - cuts[i - 1] + helper(cuts, dp, i, ind - 1) + helper(cuts, dp, ind + 1, j);
+            mini = min(mini, cost);
+        }
+        return dp[i][j] = mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        // tabulation
+        
+        int c = cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(), 0);
+        
+        sort(cuts.begin(), cuts.end());
+        vector<vector<int>> dp(c + 2, vector<int>(c + 1, 0));
+        
+        for(int i = c; i >= 1; i--) {
+            for(int j = 1; j <= c; j++) {
+                if(i > j) continue;
+                int mini = 1e9;
+                for(int ind = i; ind <= j; ind++) {
+                    int cost = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+                    mini = min(mini, cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][c];
     }
 };
