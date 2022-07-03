60aa4a15902f826cee178273197397b86909f8a9
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-04 00:24:01 +0530

    Time: 23 ms (39.82%), Space: 20.9 MB (54.10%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -29,9 +29,9 @@ public:
         if(root == NULL) return 0;
         
         int left = check(root -> left);
+        if(left == -1) return -1;
         int right = check(root -> right);
-        
-        if(left == -1 || right == -1) return -1;
+        if(right == -1) return -1;
         if(abs(left - right) > 1) return -1;
         
         return 1 + max(left, right);
