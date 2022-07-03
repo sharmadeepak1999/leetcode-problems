fdd39654a7a0b6f426bfeb839f5fc2d07ebe41c9
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-04 03:33:01 +0530

    Time: 577 ms (5.05%), Space: 96.4 MB (5.05%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -49,23 +49,6 @@ public:
     */
     
     
-    int helper(vector<int>& nums, vector<vector<vector<int>>> &dp, int i, int prevIndex, int prevSign) {
-        if(i == nums.size()) {
-            return 0;
-        }    
-        
-        if(dp[i][prevIndex + 1][prevSign + 1] != -1) return dp[i][prevIndex + 1][prevSign + 1];
-        if(prevIndex == -1) {
-            return max(1 + helper(nums, dp, i + 1, i, -1), helper(nums, dp, i + 1, -1, -1));
-        } else {
-            int currSign = nums[i] - nums[prevIndex] > 0 ? 1 : 0;
-            if((prevSign != -1 && prevSign == currSign) || nums[i] == nums[prevIndex]) {
-                return helper(nums, dp, i + 1, prevIndex, prevSign);
-            }
-           return dp[i][prevIndex + 1][prevSign + 1] = max(1 + helper(nums, dp, i + 1, i, currSign), helper(nums, dp, i + 1, prevIndex, prevSign));
-        }
-    }
-    
     int wiggleMaxLength(vector<int>& nums) {
         // tabulation
         int n = nums.size();
