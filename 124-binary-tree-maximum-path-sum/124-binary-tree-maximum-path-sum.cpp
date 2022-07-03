5c8933cd0a05da8920013a851e4157c02745f020
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-04 01:14:12 +0530

    Time: 32 ms (74.07%), Space: 27.6 MB (68.59%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -12,6 +12,7 @@
 class Solution {
 public:
     int getMaxPathSum(TreeNode* root, int &pathSum) {
+        // the intiution is similar to height/ diameter of the tree functions, we at any index, ask recursion to get us the left and righ path sum, then we check if the sum of left + right + current node is greater than the max path sum, if yes then update. we return the max path value we can get for a path passing through the current node, as current node + max(left, right). this is to tell which path should be taken to get max path sum if a path passes through the current node, we also have a check if the path sum returned by left or right is negative then we will not consider them and change them to 0, as adding negative value to our path will not give us maximum path sum.
         if(root == NULL) return 0;
         
         int left = max(0, getMaxPathSum(root -> left, pathSum));
