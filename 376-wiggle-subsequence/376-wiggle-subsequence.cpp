ed5a7fabae7815ef9b22354dc1b40aac09273301
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-04 03:32:46 +0530

    Time: 431 ms (5.05%), Space: 96.6 MB (5.05%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -22,6 +22,7 @@ public:
     }
     */
     
+    /*
     int helper(vector<int>& nums, vector<vector<vector<int>>> &dp, int i, int prevIndex, int prevSign) {
         if(i == nums.size()) {
             return 0;
@@ -44,5 +45,46 @@ public:
         int n = nums.size();
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(n + 1, vector<int>(3, -1)));
         return helper(nums, dp, 0, -1, -1);
+    }
+    */
+    
+    
+    int helper(vector<int>& nums, vector<vector<vector<int>>> &dp, int i, int prevIndex, int prevSign) {
+        if(i == nums.size()) {
+            return 0;
+        }    
+        
+        if(dp[i][prevIndex + 1][prevSign + 1] != -1) return dp[i][prevIndex + 1][prevSign + 1];
+        if(prevIndex == -1) {
+            return max(1 + helper(nums, dp, i + 1, i, -1), helper(nums, dp, i + 1, -1, -1));
+        } else {
+            int currSign = nums[i] - nums[prevIndex] > 0 ? 1 : 0;
+            if((prevSign != -1 && prevSign == currSign) || nums[i] == nums[prevIndex]) {
+                return helper(nums, dp, i + 1, prevIndex, prevSign);
+            }
+           return dp[i][prevIndex + 1][prevSign + 1] = max(1 + helper(nums, dp, i + 1, i, currSign), helper(nums, dp, i + 1, prevIndex, prevSign));
+        }
+    }
+    
+    int wiggleMaxLength(vector<int>& nums) {
+        // tabulation
+        int n = nums.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(n + 1, vector<int>(3, 0)));
+        
+        for(int i = n - 1; i >= 0; i--) {
+            for(int prevIndex = i - 1; prevIndex >= -1; prevIndex--) {
+                for(int prevSign = 1; prevSign >= -1; prevSign--) {
+                    if(prevIndex == -1) {
+                        dp[i][prevIndex + 1][prevSign + 1] = max(1 + dp[i + 1][i + 1][0], dp[i + 1][0][0]);
+                    } else {
+                        int currSign = nums[i] - nums[prevIndex] > 0 ? 1 : 0;
+                        if((prevSign != -1 && prevSign == currSign) || nums[i] == nums[prevIndex]) {
+                            dp[i][prevIndex + 1][prevSign + 1] = dp[i + 1][prevIndex + 1][prevSign + 1];
+                        }else dp[i][prevIndex + 1][prevSign + 1] = max(1 + dp[i + 1][i + 1][currSign + 1], dp[i + 1][prevIndex + 1][prevSign + 1]);
+                    }
+                }
+            }
+        }
+        return dp[0][0][0];
     }
 };
