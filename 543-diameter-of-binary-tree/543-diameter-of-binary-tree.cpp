3cd2d53aed162ad0374273c4754adcedb4c8d21c
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-07-04 00:49:10 +0530

    Time: 3 ms (99.54%), Space: 20.3 MB (26.06%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    /*
     int height(TreeNode* root) {
         if(root == NULL) return 0;
         
@@ -28,7 +29,29 @@ public:
         getDiameter(root -> right, diameter);
     }
     int diameterOfBinaryTree(TreeNode* root) {
+        // in this approach we get to each node, and calculate the diameter at that node using the left and right height, lh + rh, if it is max till now then update the diameter, and call the diameter function for the left and right child. this will take time complexity n^2, as we will compute the height of the tree for every node. 
         int diameter = INT_MIN;
+        getDiameter(root, diameter);
+        return diameter;
+    }
+    */
+    
+    int getDiameter(TreeNode* root, int &diameter) {
+        if(root == NULL) return 0;
+        
+        int lh = getDiameter(root -> left, diameter);
+        int rh = getDiameter(root -> right, diameter);
+        
+        diameter = max(diameter, lh + rh);
+        
+        return 1 + max(lh, rh);
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        // this approach is modification of the height function, instead of calculating height at every node, we can just call the height function once, and whenever it calculates the value of lh and rh we can check if there sum is the maximum so far, if yes then we can update diameter to it.
+        if(root == NULL) return 0;
+        int diameter = 0;
+        
         getDiameter(root, diameter);
         return diameter;
     }
