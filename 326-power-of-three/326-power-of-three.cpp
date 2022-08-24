3d82369dcaae6e59ebd4a423e948552fc19db555
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2022-08-24 11:58:06 +0530

    Time: 20 ms (88.73%), Space: 5.9 MB (29.00%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -8,9 +8,15 @@ public:
         return isPowerOfThree(n / 3);
     */
         
+        /*
         if(n <= 0) return false;
         double a = (double) log10(n) / log10(3);
         return floor(a) == ceil(a);
+        
+        */
+        
+        int power = pow(3, (int) (log(INT_MAX) / log(3)));
+        return n > 0 && power % n == 0;
 }
     
 };
