1e41723f9041f39def6788768748c4251a606329
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-01-03 22:10:52 +0530

    Added solution - LeetHub

---
+++ Prime Number - GFG/prime-number.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int isPrime(int n){
+        // code here
+        if(n == 1) return 0;
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.isPrime(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
