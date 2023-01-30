5cedbc30d3e5fff3b29e20cb0ed8a4cd922a2c49
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-01-30 23:08:37 +0530

    Time: 28 ms (66.25%), Space: 13.7 MB (30.63%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isComponentBipartite(vector<vector<int>>& graph, vector<int> &type, int nodeOrg) {
+        queue<int> q;
+        q.push(nodeOrg);
+        
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            
+            for(auto &adj:graph[node]) {
+                if(type[adj] == -1) {
+                    type[adj] = type[node] == 1 ? 0 : 1;
+                    q.push(adj);
+                }
+                else if(type[adj] != -1 && type[adj] == type[node]) return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        
+        vector<int> type(n, -1);
+        
+        for(int i = 0; i < n; i++) {
+            if(type[i] == -1 && !isComponentBipartite(graph, type, i)) return false;
+        }
+        return true;
+    }
+};
