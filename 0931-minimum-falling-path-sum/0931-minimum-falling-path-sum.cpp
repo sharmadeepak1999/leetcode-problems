12535c9b28fdcbb986337756bfd0ef7a21bf18fe
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-03-08 22:43:46 +0530

    Time: 16 ms (61.08%), Space: 10.7 MB (6.50%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -4,21 +4,24 @@ public:
         int n = matrix.size();
         int mini = INT_MAX;
         vector<vector<int>> dp(n, vector<int>(n, 1e9));
+        vector<int> front(n, 1e9);
         for(int i = 0; i < n; i++) {
-            dp[n - 1][i] = matrix[n - 1][i];
+            front[i] = matrix[n - 1][i];
         }
         for(int i = n - 2; i >= 0; i--) {
+            vector<int> curr(n, 1e9);
             for(int j = 0; j < n; j++) {
                 int ldiag = 1e9, rdiag = 1e9, down = 1e9;
                 
-                if(j > 0) ldiag = dp[i + 1][j - 1];
-                down = dp[i + 1][j];
-                if(j < n - 1) rdiag = dp[i + 1][j + 1];
-                dp[i][j] = matrix[i][j] + min(min(ldiag, rdiag), down);
+                if(j > 0) ldiag = front[j - 1];
+                down = front[j];
+                if(j < n - 1) rdiag = front[j + 1];
+                curr[j] = matrix[i][j] + min(min(ldiag, rdiag), down);
             }
+            front = curr;
         }
         for(int i = 0; i < n; i++) {
-            mini = min(mini, dp[0][i]);
+            mini = min(mini, front[i]);
         }
         return mini;
     }
