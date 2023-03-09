db14ae57c28359a420fcf74636173557b2ffadf3
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-03-09 17:14:58 +0530

    Time: 397 ms (37.58%), Space: 94.2 MB (43.22%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -4,17 +4,20 @@ public:
         int n = nums.size();
         int osum = accumulate(nums.begin(), nums.end(), 0);
         if(osum % 2 == 1) return false;
-        vector<vector<int>> dp(n, vector<int>(osum / 2 + 1, 0));
-        for(int i = 0; i < n; i++) dp[i][0] = true;
-        
+        int target = osum / 2;
+        vector<int> prev(target + 1, 0);
+        prev[0] = true;
         for(int i = 1; i < n; i++) {
+            vector<int> curr(target + 1, 0);
+            curr[0] = true;
             for(int j = 1; j <= osum / 2; j++) {
-                int nottake = dp[i - 1][j];
+                int nottake = prev[j];
                 int take = 0;
-                if(j >= nums[i]) take = dp[i - 1][j - nums[i]];
-                dp[i][j] = take or nottake;
+                if(j >= nums[i]) take = prev[j - nums[i]];
+                curr[j] = take or nottake;
             }
+            prev = curr;
         }
-        return dp[n - 1][osum / 2];
+        return prev[osum / 2];
     }
 };
