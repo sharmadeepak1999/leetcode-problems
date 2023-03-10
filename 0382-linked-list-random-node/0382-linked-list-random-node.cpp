9ecac5df69b70dfe708c7a8f28d9f6d0969daa12
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-03-10 12:27:51 +0530

    Time: 26 ms (48.03%), Space: 16.6 MB (85.75%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -9,23 +9,23 @@
  * };
  */
 class Solution {
-    vector<int> mp;
 public:
+    ListNode *temp;
     Solution(ListNode* head) {
-        ListNode *temp = head;
-        while(temp) {
-            mp.push_back(temp -> val);
-            temp = temp -> next;
-        }
+        temp = head;
     }
     
     int getRandom() {
-        const int range_from  = 0;
-        const int range_to = mp.size() - 1;
-        std::random_device rand_dev;
-        std::mt19937 generator(rand_dev());
-        std::uniform_int_distribution<int> distr(range_from, range_to);
-        return mp[distr(generator)];
+        ListNode *curr = temp;
+        
+        int res = 0; int n = 1;
+        
+        while(curr) {
+            if(rand() % n == 0) res = curr -> val;
+            n++;
+            curr = curr -> next;
+        }
+        return res;
     }
 };
 
