2c10372ae316fe39bab1f9e3b98a42e97a75734c
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-03-10 16:06:13 +0530

    Time: 385 ms (33.38%), Space: 8.9 MB (96.35%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int helper(vector<int> &nums, int target, int sum, int i) {
+    int helper(vector<int> &nums, int target, int i) {
         if(i == 0) {
-            int cnt = 0;
-            if(sum + nums[0] == target) cnt++;
-            if(sum - nums[0] == target) cnt++;
-            return cnt;
+            if(target == 0 && nums[0] == 0) return 2;
+            if(target == 0 || nums[0] == target) return 1;
+            return 0;
         }
-        int plus = helper(nums, target, sum + nums[i], i - 1);
-        int minus = helper(nums, target, sum - nums[i], i - 1);
+        int nottake = helper(nums, target, i - 1);
+        int take = 0;
+        if(nums[i] <= target) take = helper(nums, target - nums[i], i - 1);
         
-        return plus + minus;
+        return nottake + take;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        return helper(nums, target, 0, n - 1);
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if((total - target) < 0 || (total - target) % 2 == 1) return 0;
+        int sum = (total - target) / 2;
+        return helper(nums, sum, n - 1);
     }
 };
