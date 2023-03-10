1c83faa220b93c8d8a5c5c2a4af44b975110cbf9
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-03-10 16:15:42 +0530

    Time: 14 ms (75.30%), Space: 12.1 MB (46.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int helper(vector<int> &nums, vector<vector<int>> &dp, int target, int i) {
-        if(i == 0) {
-            if(target == 0 && nums[0] == 0) return 2;
-            if(target == 0 || nums[0] == target) return 1;
-            return 0;
-        }
-        if(dp[i][target] != -1) return dp[i][target];
-        
-        int nottake = helper(nums, dp, target, i - 1);
-        int take = 0;
-        if(nums[i] <= target) take = helper(nums, dp, target - nums[i], i - 1);
-        
-        return dp[i][target] = nottake + take;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int total = accumulate(nums.begin(), nums.end(), 0);
         if((total - target) < 0 || (total - target) % 2 == 1) return 0;
         int sum = (total - target) / 2;
-        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-        return helper(nums, dp, sum, n - 1);
+        vector<vector<int>> dp(n, vector<int>(sum + 1, 0));
+        for(int si = 0; si <= sum; si++) {
+            if(si == 0 && nums[0] == 0) dp[0][si] = 2;
+            else if(si == 0 || nums[0] == si) dp[0][si] = 1;
+        }
+        for(int i = 1; i < n; i++) {
+            for(int si = 0; si <= sum; si++) {
+                int nottake = dp[i - 1][si];
+                int take = 0;
+                if(nums[i] <= si) take = dp[i - 1][si - nums[i]];
+                dp[i][si] = nottake + take;
+            }
+        }
+        return dp[n - 1][sum];
     }
 };
