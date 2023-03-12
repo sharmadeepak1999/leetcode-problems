df6185ec5562f214c926b1d1179857d7bcf18f04
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-03-12 11:56:40 +0530

    Time: 42 ms (33.56%), Space: 30 MB (5.28%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,30 +10,40 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode *newHead = new ListNode();
-        ListNode *temp = newHead;
-        while(1) {
-            bool f = false;
-            int k = lists.size();
-            int i = 0;
-            int mini = INT_MAX;
-            int idx = -1;
-            while(i < k) {
-                if(lists[i] && lists[i] -> val < mini) {
-                    f = true;
-                    mini = lists[i] -> val;
-                    idx = i;
-                }
-                i++;
+    ListNode* mergeTwoLists(ListNode* left, ListNode* right) {
+        if(!left) return right;
+        if(!right) return left;
+        
+        ListNode* temp1 = left;
+        ListNode* temp2 = right;
+        ListNode* dummy = new ListNode();
+        ListNode* temp = dummy;
+        while(temp1 && temp2) {
+            temp = temp -> next = new ListNode();
+            if(temp1 -> val <= temp2 -> val) {
+                temp -> val = temp1 -> val;
+                temp1 = temp1 -> next;
+            } else {
+                temp -> val = temp2 -> val;
+                temp2 = temp2 -> next;
             }
-            if(f) {
-                i = 0;
-                temp = temp -> next = new ListNode(mini);
-                lists[idx] = lists[idx] -> next;
-            }
-            else break;
         }
-        return newHead -> next;
+        if(temp1) temp -> next = temp1;
+        if(temp2) temp -> next = temp2;
+
+        return dummy -> next;
+    }
+    ListNode* merge(vector<ListNode*> &lists, int s, int e) {
+        if(lists.empty() || s > e) return NULL;
+        if(e == s) return lists[s];
+        int mid = (s + e) / 2;
+        
+        ListNode* left = merge(lists, s, mid);
+        ListNode* right = merge(lists, mid + 1, e);
+        
+        return mergeTwoLists(left, right);
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return merge(lists, 0, lists.size() - 1);
     }
 };
