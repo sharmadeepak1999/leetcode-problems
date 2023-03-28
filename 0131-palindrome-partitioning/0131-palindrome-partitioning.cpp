679b42f64d0f95c87ffa2e4f0cb4d67136d57428
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-03-28 21:55:20 +0530

    Time: 607 ms (5.01%), Space: 362.3 MB (5.01%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    void helper(string s, string curr, int i, vector<vector<string>> &ans, vector<string> currPart) {
-        if(i == s.size()) {
-            string orgS = "";
-            for(auto &part:currPart) orgS += part;
+    void helper(string s, vector<vector<string>> &ans, vector<string> curr, string str, int i) {
+        if(i == s.length()) {
+            if(str != "")
+            curr.push_back(str);
             
-            if(orgS == s) {
-                ans.push_back(currPart);
+            bool f = true;
+            
+            for(auto &st:curr) {
+                string temp = st;
+                reverse(temp.begin(), temp.end());
+                if(temp != st) {
+                    f = false;
+                    break;
+                }
             }
+            if(f) ans.push_back(curr);
             return;
         }
-        curr += s[i];
-        string revcurr = curr;
-        reverse(revcurr.begin(), revcurr.end());
-        
-        if(curr == revcurr) {
-            currPart.push_back(curr);
-            helper(s, "", i + 1, ans, currPart);
-            currPart.pop_back();
+        if(str != "") {
+            curr.push_back(str);
+            helper(s, ans, curr, "", i);
+            curr.pop_back();
         }
-        helper(s, curr, i + 1, ans, currPart);
+        helper(s, ans, curr, str + s[i], i + 1);
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> currPart;
-        
-        helper(s, "", 0, ans, currPart);
+        vector<string> curr;
+        string str = "";
+        helper(s, ans, curr, str, 0);
         return ans;
     }
 };
