669f52e2c5980eee81778aacb0416ac4ae0889d7
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-03-29 14:23:36 +0530

    Time: 34 ms (21.03%), Space: 45.8 MB (8.84%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,51 +1,40 @@
 class Solution {
 public:
-    bool isPossible(vector<vector<int>> &board, int row, int col, int n) {
+    bool canPlace(vector<string> board, int n, int row, int col) {
         for(int i = row - 1; i >= 0; i--) {
-            if(board[i][col] == 1) return false;
+            if(board[i][col] == 'Q') return false;
         }
-        for(int i = col - 1; i >= 0; i--) {
-            if(board[row][i] == 1) return false;
+        
+        for(int j = col - 1; j >= 0; j--) {
+            if(board[row][j] == 'Q') return false;
         }
         
         for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if(board[i][j] == 1) return false;
+            if(board[i][j] == 'Q') return false;
         }
         
         for(int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if(board[i][j] == 1) return false;
+            if(board[i][j] == 'Q') return false;
         }
         return true;
     }
-    
-    void helper(vector<vector<int>> &board, vector<vector<string>> &solutions, int row, int n) {
+    void helper(vector<vector<string>> &ans, vector<string> &board, int n, int row) {
         if(row == n) {
-            vector<string> sol;
-            for(int i = 0; i < n; i++) {
-                string r = "";
-                for(int j = 0; j < n; j++) {
-                    if(board[i][j] == 1) r += 'Q';
-                    else r += '.';
-                }
-                sol.push_back(r);
-            }
-            solutions.push_back(sol);
+            ans.push_back(board);
             return;
         }
-        
-        
-        for(int i = 0; i < n; i++) {
-            if(isPossible(board, row, i, n)) {
-                board[row][i] = 1;
-                helper(board, solutions, row + 1, n);
-                board[row][i] = 0;
+        for(int j = 0; j < n; j++) {
+            if(canPlace(board, n, row, j)) {
+                board[row][j] = 'Q';
+                helper(ans, board, n, row + 1);
+                board[row][j] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<int>> board(10, vector<int>(10, 0));
-        vector<vector<string>> solutions;
-        helper(board, solutions, 0, n);
-        return solutions;
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+        helper(ans, board, n, 0);
+        return ans;
     }
 };
