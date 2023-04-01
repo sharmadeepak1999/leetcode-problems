8afb12be4b84a3645c89649037c807bf93c3a24c
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-04-01 17:00:26 +0530

    Time: 25 ms (81.61%), Space: 6.5 MB (55.48%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    bool isPossible(vector<vector<char>> &board, int row, int col, char c) {
-        for(int i = 0; i < 9; i++) {
-            if(board[i][col] == c) return false;
-            if(board[row][i] == c) return false;
-            if(board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == c) return false;
+    bool canPlace(vector<vector<char>> &board, int i, int j, char n) {
+        for(int k = 0; k < 9; k++) {
+            if(board[i][k] == n) return false;
+            if(board[k][j] == n) return false;
+            if(board[3 * (i / 3) + (k / 3)][3 * (j / 3) + (k % 3)] == n) return false;
+
         }
+        
         return true;
     }
     bool helper(vector<vector<char>> &board) {
-        for(int i = 0; i < board.size(); i++) {
-            for(int j = 0; j < board[i].size(); j++) {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
                 if(board[i][j] == '.') {
-                    for(char c = '1'; c <= '9'; c++) {
-                        if(isPossible(board, i, j, c)) {
-                            board[i][j] = c;
+                    for(char k = '1'; k <= '9'; k++) {
+                        if(canPlace(board, i, j, k)) {
+                            board[i][j] = k;
                             if(helper(board)) return true;
                             board[i][j] = '.';
                         }
@@ -26,6 +28,6 @@ public:
         return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
-         helper(board);
+        helper(board);
     }
 };
