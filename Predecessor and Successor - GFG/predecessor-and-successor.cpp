018eaaa19aef124581cf19fbdd2ff83db65411c5
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-04-08 02:09:49 +0530

    Added solution - LeetHub

---
+++ Predecessor and Successor - GFG/predecessor-and-successor.cpp
@@ -0,0 +1,147 @@
+//{ Driver Code Starts
+// C++ program to find predecessor and successor in a BST
+#include <iostream>
+using namespace std;
+
+// BST Node
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    key = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+int key=0;
+// This function finds predecessor and successor of key in BST.
+// It sets pre and suc as predecessor and successor respectively
+void findPreSuc(Node* root, Node*& pre, Node*& suc, int key);
+
+ void insert(struct Node *root,int n1,int n2,char lr)
+ {
+     if(root==NULL)
+        return;
+     if(root->key==n1)
+     {
+         switch(lr)
+         {
+          case 'L': root->left=new Node(n2);
+                    break;
+          case 'R': root->right=new Node(n2);
+                    break;
+         }
+     }
+     else
+     {
+         insert(root->left,n1,n2,lr);
+         insert(root->right,n1,n2,lr);
+     }
+ }
+// Driver program to test above functions
+int main()
+{
+    /* Let us construct the tree shown in above diagram */
+    int t,k;
+    cin>>t;
+    while(t--)
+    {
+    int n;
+    cin>>n;
+    struct Node *root=NULL;
+    Node *pre=NULL;
+    Node *suc=NULL;
+    while(n--)
+    {
+        char lr;
+        int n1,n2;
+        cin>>n1>>n2;
+        cin>>lr;
+        if(root==NULL)
+        {
+            root=new Node(n1);
+            switch(lr){
+                    case 'L': root->left=new Node(n2);
+                    break;
+                    case 'R': root->right=new Node(n2);
+                    break;
+                    }
+        }
+        else
+        {
+            insert(root,n1,n2,lr);
+        }
+    }
+   // Inorder(root);
+    //Node * target =ptr;
+    //printkdistanceNode(root, target, k);
+    //cout<<endl;
+    cin>>key;
+    findPreSuc(root, pre, suc, key);
+	if (pre != NULL)
+	cout << pre->key;
+	else
+	cout << "-1";
+
+	if (suc != NULL)
+	cout <<" "<<suc->key<<endl;
+	else
+	cout << " "<<"-1"<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* BST Node
+struct Node
+{
+	int key;
+	struct Node *left, *right;
+};
+*/
+
+// This function finds predecessor and successor of key in BST.
+// It sets pre and suc as predecessor and successor respectively
+#include<bits/stdc++.h>
+
+void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
+{
+
+// Your code goes here
+    vector<Node*> inorder;
+    
+    Node* curr = root;
+    
+    while(curr) {
+        if(!curr -> left) {
+            inorder.push_back(curr);
+            curr = curr -> right;
+        } else {
+            Node* temp = curr -> left;
+            
+            while(temp -> right && temp -> right != curr) temp = temp -> right;
+            
+            if(temp -> right) {
+                temp -> right = NULL;
+                inorder.push_back(curr);
+                curr = curr -> right;
+            } else {
+                temp -> right = curr;
+                curr = curr -> left;
+            }
+        }
+    }
+    int n = inorder.size();
+    for(int i = 0; i < n; i++) {
+        if(inorder[i] -> key < key) pre = inorder[i];
+        if(inorder[i] -> key > key) {
+            suc = inorder[i];
+            break;
+        }
+    }
+}
