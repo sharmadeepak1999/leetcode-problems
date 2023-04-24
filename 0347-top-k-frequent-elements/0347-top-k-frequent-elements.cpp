c55d32894cdc18181b120df8abdf0da2ada1faef
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-04-24 19:15:33 +0530

    Time: 20 ms (36.43%), Space: 15.5 MB (7.60%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        /*
+        // Using heap
         unordered_map<int, int> freq;
         
         for(auto &num:nums) freq[num]++;
@@ -18,6 +20,37 @@ public:
             pq.pop();
         }
         
+        return ans;
+        */
+        
+        // bucket sort
+        
+        unordered_map<int, int> freq;
+        
+        for(auto &num:nums) freq[num]++;
+        
+        vector<vector<int>> bucket(nums.size() + 1);
+        
+        
+        for(auto &it:freq) {
+            bucket[it.second].push_back(it.first);
+        }
+        vector<int> ans;
+        
+        int i = bucket.size() - 1;
+        
+        while(i >= 0) {
+            while(i >= 0 && bucket[i].size() == 0) i--;
+            if(i >= 0) {
+                for(int j = 0; j < bucket[i].size(); j++) {
+                    k--;
+                    ans.push_back(bucket[i][j]);
+                    if(k == 0) break;
+                }
+            }
+            if(k == 0) break;
+            i--;
+        }
         return ans;
     }
 };
