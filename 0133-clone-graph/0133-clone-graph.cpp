18b2f4db94188eb3a3132fdd2b306c633724d4fa
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-04-24 15:57:10 +0530

    Time: 5 ms (67.18%), Space: 9 MB (29.15%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,31 +1,42 @@
-class Solution {
-    public:
-    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
-    {
-        vector<Node*> neighbour;
-        Node* clone=new Node(cur->val);
-        mp[cur]=clone;
-            for(auto it:cur->neighbors)
-            {
-                if(mp.find(it)!=mp.end())   //already clone and stored in map
-                {
-                    neighbour.push_back(mp[it]);    //directly push back the clone node from map to neigh
-                }
-                else
-                    neighbour.push_back(dfs(it,mp));
-            }
-            clone->neighbors=neighbour;
-            return clone;
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
     }
-    Node* cloneGraph(Node* node) {
-        unordered_map<Node*,Node*> mp;
-        if(node==NULL)
-            return NULL;
-        if(node->neighbors.size()==0)   //if only one node present no neighbors
-        {
-            Node* clone= new Node(node->val);
-            return clone; 
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    
+    unordered_map<Node*, Node*> mp;
+        
+    Node* dfs(Node* node) {
+        if(!node) return NULL;
+        Node* newNode = new Node(node -> val);
+        mp[node] = newNode;
+        
+        for(auto &adjNode:node -> neighbors) {
+            if(mp.find(adjNode) == mp.end()) {
+                newNode -> neighbors.push_back(dfs(adjNode));
+            } else newNode -> neighbors.push_back(mp[adjNode]);
         }
-        return dfs(node,mp);
+        return newNode;
+    }
+public:
+    Node* cloneGraph(Node* node) {
+        return dfs(node); 
     }
 };
