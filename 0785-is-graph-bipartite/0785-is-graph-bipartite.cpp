3fcc118393f4b0ab7852f6e1e3ed1442baec8f99
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-04-24 11:53:32 +0530

    Time: 32 ms (21.34%), Space: 13.5 MB (87.05%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,42 +1,11 @@
 class Solution {
 public:
-    /*
-    bool isComponentBipartite(vector<vector<int>>& graph, vector<int> &type, int nodeOrg) {
-        queue<int> q;
-        q.push(nodeOrg);
-        
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            
-            for(auto &adj:graph[node]) {
-                if(type[adj] == -1) {
-                    type[adj] = type[node] == 1 ? 0 : 1;
-                    q.push(adj);
-                }
-                else if(type[adj] != -1 && type[adj] == type[node]) return false;
-            }
-        }
-        return true;
-    }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        
-        vector<int> type(n, -1);
-        
-        for(int i = 0; i < n; i++) {
-            if(type[i] == -1 && !isComponentBipartite(graph, type, i)) return false;
-        }
-        return true;
-    }
-    */
-    
-    bool isComponentBipartite(vector<vector<int>> &graph, vector<int> &type, int node, int prevType) {
+    bool dfs(vector<vector<int>> &graph, vector<int> &type, int node, int prevType) {
         type[node] = !prevType;
         
-        for(auto &adj:graph[node]) {
-            if(type[adj] == -1 && !isComponentBipartite(graph, type, adj, !prevType)) return false;
-            if(type[adj] != -1 && type[adj] == type[node]) return false;
+        for(auto &adjNode:graph[node]) {
+            if(type[adjNode] == type[node]) return false;
+            if(type[adjNode] == -1 && !dfs(graph, type, adjNode, !prevType)) return false;
         }
         return true;
     }
@@ -44,9 +13,11 @@ public:
         int n = graph.size();
         
         vector<int> type(n, -1);
-        int prevType = 0;
+        
         for(int i = 0; i < n; i++) {
-            if(type[i] == -1 && !isComponentBipartite(graph, type, i, prevType)) return false;
+            if(type[i] == -1) {
+                if(!dfs(graph, type, i, 0)) return false;
+            }
         }
         return true;
     }
