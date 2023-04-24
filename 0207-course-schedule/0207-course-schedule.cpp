76896a7bcbce2909a8d16e588f10bab197c1f141
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-04-24 11:04:16 +0530

    Time: 20 ms (82.93%), Space: 13.4 MB (76.20%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
+    bool detectCycle(vector<int> adj[], vector<int> &visited, vector<int> &pathVisited, int node) {
+        visited[node] = 1;
+        pathVisited[node] = 1;
+        
+        for(auto &adjNode:adj[node]) {
+            if(!visited[adjNode]) {
+                if(detectCycle(adj, visited, pathVisited, adjNode)) return true;
+            } else if(pathVisited[adjNode]) return true;
+        }
+        pathVisited[node] = 0;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // apply topo sort technique 
+        vector<int> adj[numCourses];
         
-        int n = numCourses;
-        vector<vector<int>> adj(n);
-        for(auto &p:prerequisites) {
-            adj[p[0]].push_back(p[1]);
+        for(auto &it:prerequisites) {
+            adj[it[0]].push_back(it[1]);
         }
         
-        vector<int> indegree(n, 0);
+        vector<int> visited(numCourses, 0), pathVisited(numCourses, 0);
         
-        for(int i = 0; i < n; i++) {
-            for(auto &it:adj[i]) {
-                indegree[it]++;
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i]) {
+                if(detectCycle(adj, visited, pathVisited, i)) return false;
             }
         }
-        
-        queue<int> q;
-        
-        for(int i = 0; i < n; i++) {
-            if(indegree[i] == 0) q.push(i);
-        }
-        
-        int count = 0;
-        
-        while(!q.empty()) {
-            int course = q.front();
-            q.pop();
-            count++;
-            
-            for(auto &it:adj[course]) {
-                indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
-            }
-        }
-        return count == n;
+        return true;
     }
 };
