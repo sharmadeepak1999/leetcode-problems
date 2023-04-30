79d4ebd8a59a70bf800420011bee4e194939c1bf
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-04-30 15:07:26 +0530

    Time: 502 ms (95.29%), Space: 138.9 MB (89.49%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,29 +1,91 @@
+class Disjoint {
+    
+    public:
+    vector<int> rank, parent;
+        Disjoint(int n) {
+            rank.resize(n + 1, 0);
+            parent.resize(n + 1);
+            for(int i = 1; i <= n; i++) {
+                parent[i] = i;
+            }
+        }
+        int findUPar(int u) {
+            if(parent[u] == u) return u;
+            
+            return parent[u] = findUPar(parent[u]);
+        }
+        void unionByRank(int u, int v) {
+            int pu = findUPar(u);
+            int pv = findUPar(v);
+            
+            if(pu == pv) return;
+            
+            if(rank[pu] < rank[pv]) {
+                parent[pu] = pv;
+            } else if(rank[pv] < rank[pu]) {
+                parent[pv] = pu;
+            } else {
+                parent[pu] = pv;
+                rank[pv]++;
+            }
+        }
+    
+        bool isConnected() {
+            int count = 0;
+            for(int i = 1; i < parent.size(); i++) {
+                if(parent[i] == i) count++;
+            }
+            return count == 1;
+        }
+};
 class Solution {
 public:
-    int find(vector<int> &ds, int i) {
-    return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
-}
-int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-    vector<int> ds_both(n + 1, -1);
-    int used = 0;
-    for (int type = 3; type > 0; --type) {
-        auto ds_one = ds_both;
-        auto &ds = type == 3 ? ds_both : ds_one;
-        for (auto &e : edges)
-            if (e[0] == type) {
-                int i = find(ds, e[1]), j = find(ds, e[2]);
-                if (i != j) {
-                    ++used;
-                    if (ds[j] < ds[i])
-                        swap(i, j);
-                    ds[i] += ds[j];
-                    ds[j] = i;
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        Disjoint aliceDS(n);
+        Disjoint bobDS(n);
+        int count = 0;
+        for(auto &edge:edges) {
+            int type = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+            
+            if(type == 3) {
+                bool req = false;
+                if(aliceDS.findUPar(u) != aliceDS.findUPar(v)) {
+                    req = true;
+                    aliceDS.unionByRank(u, v);
+                }
+                if(bobDS.findUPar(u) != bobDS.findUPar(v)) {
+                    req = true;
+                    bobDS.unionByRank(u, v);
+                }
+                if(req) count++;
+            }
+        }
+        
+        for(auto &edge:edges) {
+            int type = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+            
+            if(type == 1) {
+                if(aliceDS.findUPar(u) != aliceDS.findUPar(v)) {
+                    count++;
+                    aliceDS.unionByRank(u, v);
                 }
             }
-        if (type != 3 && ds[find(ds, 1)] != -n)
-            return -1;
+            if(type == 2) {
+                if(bobDS.findUPar(u) != bobDS.findUPar(v)) {
+                    count++;
+                    bobDS.unionByRank(u, v);
+                }
+            }
+        }
+        
+        if(aliceDS.isConnected() && bobDS.isConnected()) {
+            return edges.size() - count;
+        }
+        
+        return -1;
     }
-    return edges.size() - used;
-}
-
 };
