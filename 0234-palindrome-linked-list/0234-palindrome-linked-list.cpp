cd056714a77349595c40e90c26f7e39db5c08681
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-06-05 19:35:35 +0530

    Time: 254 ms (58.83%), Space: 114.2 MB (80.01%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -9,18 +9,31 @@
  * };
  */
 class Solution {
-    bool helper(ListNode* head, ListNode *&left) {
-        if(head == NULL) return true;
-        bool res = helper(head -> next, left);
-        if(!res) return false;
-        if(head -> val == left -> val) {
-            left = left -> next;
-            return true;
+    ListNode* reverseLL(ListNode* head) {
+        ListNode *prev = NULL, *temp = head;
+        while(temp) {
+            ListNode* t = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = t;
         }
-        return false;
+        return prev;
     }
 public:
     bool isPalindrome(ListNode* head) {
-        return helper(head, head);
+        ListNode *slow = head, *fast = head;
+        
+        while(fast && fast -> next && fast -> next -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        ListNode *temp1 = head, *temp2 = reverseLL(slow -> next);
+        slow -> next = NULL;
+        while(temp1 && temp2) {
+            if(temp1 -> val != temp2 -> val) return false;
+            temp1 = temp1 -> next;
+            temp2 = temp2 -> next;
+        }
+        return true;
     }
 };
