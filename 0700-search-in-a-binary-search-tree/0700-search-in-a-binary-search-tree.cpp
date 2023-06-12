5a9c66d5043de19abf765b31d2d100322cfd5549
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-06-12 12:19:03 +0530

    Time: 48 ms (39.85%), Space: 34.8 MB (45.67%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -15,7 +15,7 @@ public:
         if(root == NULL) return NULL;
         
         if(root -> val == val) return root;
-        if(root -> val > val) return searchBST(root -> left, val);
+        else if(root -> val > val) return searchBST(root -> left, val);
         return searchBST(root -> right, val);
     }
 };
