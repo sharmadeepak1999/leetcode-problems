b0f960a34e640d965e50affce656f5e2a9002cbd
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-06-13 17:12:02 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -7,29 +7,27 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int v, vector<vector<int>> adj[])
+    int spanningTree(int n, vector<vector<int>> adj[])
     {
         // code here
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> visited(v, 0);
         
+        pq.push({ 0, 0 });
+        vector<int> visited(n, 0);
         int sum = 0;
-        
-        pq.push({0, 0});
         while(!pq.empty()) {
-            int w = pq.top().first;
+            int wt = pq.top().first;
             int node = pq.top().second;
             pq.pop();
-            
-            if(visited[node] == 1) continue;
-            
-            sum += w;
+            if(visited[node]) continue;
+            sum += wt;
             visited[node] = 1;
             for(auto &it:adj[node]) {
-                int edgeW = it[1];
+                int adjWt = it[1];
                 int adjNode = it[0];
+                
                 if(!visited[adjNode]) {
-                    pq.push({ edgeW, adjNode });
+                    pq.push({ adjWt, adjNode });
                 }
             }
         }
