7d9a567f89e908ceb6f6958a68e0bce728f6609e
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-06-13 15:02:53 +0530

    Time: 27 ms (64.72%), Space: 13.6 MB (52.97%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,22 +1,32 @@
 class Solution {
-public:
-    bool dfs(vector<vector<int>> &graph, vector<int> &type, int node, int prevType) {
-        type[node] = !prevType;
-        
-        for(auto &adjNode:graph[node]) {
-            if(type[adjNode] == type[node]) return false;
-            if(type[adjNode] == -1 && !dfs(graph, type, adjNode, !prevType)) return false;
+    bool bfs(vector<vector<int>>& graph, vector<int> &visited, vector<int> &color, int root) {
+        queue<int> q;
+        q.push(root);
+        visited[root] = 1;
+        color[root] = 0;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for(auto &adjNode:graph[node]) {
+                if(!visited[adjNode]) {
+                    visited[adjNode] = 1;
+                    color[adjNode] = !color[node];
+                    q.push(adjNode);
+                } else if(color[adjNode] == color[node]) return false;
+            }
         }
         return true;
     }
+public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        
-        vector<int> type(n, -1);
+        vector<int> color(n, -1);
+        vector<int> visited(n, 0);
         
         for(int i = 0; i < n; i++) {
-            if(type[i] == -1) {
-                if(!dfs(graph, type, i, 0)) return false;
+            if(!visited[i]) {
+                if(!bfs(graph, visited, color, i)) return false;
             }
         }
         return true;
