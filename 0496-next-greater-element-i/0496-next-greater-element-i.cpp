687ffe02c2a14c9c6172c010511b822407798cbc
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-06-15 17:25:54 +0530

    Time: 3 ms (95.34%), Space: 8.9 MB (33.37%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> s;
-        
-        s.push(-1);
-        
+        stack<int> st;
+        st.push(-1);
         unordered_map<int, int> mp;
-        int n = nums2.size();
-        for(int i = n - 1; i >= 0; i--) {
-            while(nums2[i] > s.top() && s.top() != -1) s.pop();
-            
-            mp[nums2[i]] = s.top();
-            s.push(nums2[i]);
+        int n2 = nums2.size();
+        for(int i = n2 - 1; i >= 0; i--) {
+            while(st.top() != -1 && st.top() < nums2[i]) {
+                st.pop();
+            }
+            mp[nums2[i]] = st.top();
+            st.push(nums2[i]);
         }
         
         vector<int> ans;
         
-        for(auto &num:nums1) ans.push_back(mp[num]);
+        for(auto &num:nums1) {
+            ans.push_back(mp[num]);
+        }
         return ans;
     }
 };
