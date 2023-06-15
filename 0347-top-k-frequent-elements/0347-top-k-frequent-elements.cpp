a5be545848750b0596f95a67e0ed51c865d6ae4f
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-06-15 11:57:24 +0530

    Time: 4 ms (99.95%), Space: 13.7 MB (56.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,55 +1,19 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        /*
-        // Using heap
-        unordered_map<int, int> freq;
+        unordered_map<int, int> mp;
         
-        for(auto &num:nums) freq[num]++;
+        for(auto &num:nums) mp[num]++;
         
         priority_queue<pair<int, int>> pq;
         
-        for(auto &it:freq) {
-            pq.push({ it.second, it.first });
-        }
+        for(auto &it:mp) pq.push({ it.second, it.first });
         
         vector<int> ans;
         
-        for(int i = 1; i <= k; i++) {
+        while(k--) {
             ans.push_back(pq.top().second);
             pq.pop();
-        }
-        
-        return ans;
-        */
-        
-        // bucket sort
-        
-        unordered_map<int, int> freq;
-        
-        for(auto &num:nums) freq[num]++;
-        
-        vector<vector<int>> bucket(nums.size() + 1);
-        
-        
-        for(auto &it:freq) {
-            bucket[it.second].push_back(it.first);
-        }
-        vector<int> ans;
-        
-        int i = bucket.size() - 1;
-        
-        while(i >= 0) {
-            while(i >= 0 && bucket[i].size() == 0) i--;
-            if(i >= 0) {
-                for(int j = 0; j < bucket[i].size(); j++) {
-                    k--;
-                    ans.push_back(bucket[i][j]);
-                    if(k == 0) break;
-                }
-            }
-            if(k == 0) break;
-            i--;
         }
         return ans;
     }
