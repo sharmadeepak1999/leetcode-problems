26da19b1afd186123ede209ec055a59afd73bc48
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-06-21 00:47:06 +0530

    Time: 11 ms (88.36%), Space: 15.9 MB (70.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int m1 = INT_MIN, m2 = INT_MIN, c1 = 0, c2 = 0;
-        int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            if(m1 == nums[i]) {
-                c1++;
-            } else if(m2 == nums[i]) {
-                c2++;
-            } else if(c1 == 0) {
-                m1 = nums[i];
-                c1 = 1;
-            } else if(c2 == 0) {
-                m2 = nums[i];
-                c2 = 1;
+        int count1 = 0, count2 = 0;
+        int e1 = INT_MIN, e2 = INT_MIN;
+        for(auto &num:nums) {
+            if(e1 == num) count1++;
+            else if(e2 == num) count2++;
+            else if(count1 == 0){
+                e1 = num;
+                count1++;
+            } else if(count2 == 0) {
+                e2 = num;
+                count2++;
             } else {
-                c1--;
-                c2--;
+                count1--;
+                count2--;
             }
         }
-        
-        vector<int> ans;
-        
-        c1 = 0, c2 = 0;
-        
+        int n = nums.size();
+        count1 = 0;
+        count2 = 0;
         for(int i = 0; i < n; i++) {
-            if(nums[i] == m1) c1++;
-            if(nums[i] == m2) c2++;
+            if(e1 == nums[i]) count1++;
+            else if(e2 == nums[i]) count2++;
         }
-        
-        if(c1 > n / 3) ans.push_back(m1);
-        if(c2 > n / 3) ans.push_back(m2);
-        
+        vector<int> ans;
+        if(count1 > n / 3) ans.push_back(e1);
+        if(count2 > n / 3) ans.push_back(e2);
+
         return ans;
     }
 };
