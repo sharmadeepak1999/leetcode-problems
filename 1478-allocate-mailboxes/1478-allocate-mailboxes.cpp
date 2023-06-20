1a0a37b1f3a4564887a65d8cbbd61cc1ba2c82d6
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-06-20 18:25:02 +0530

    Time: 250 ms (5.99%), Space: 40.1 MB (5.53%) - LeetHub

--- 1478-allocate-mailboxes/1478-allocate-mailboxes.cpp
+++ 1478-allocate-mailboxes/1478-allocate-mailboxes.cpp
@@ -3,24 +3,26 @@ public:
     int minDistance(vector<int>& houses, int k) {
         int n = houses.size();
         sort(houses.begin(), houses.end());
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(n + 1, vector<int>(k + 1, INT_MAX)));
+        vector<vector<int>> front(n + 1, vector<int>(k + 1, INT_MAX));
         for(int i = 0; i <= n; i++) {
-            dp[i][n][0] = 0;
+            front[i][0] = 0;
         }
         
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = n - 1; j >= 0; j--) {
+        for(int j = n - 1; j >= 0; j--) {
+            vector<vector<int>> temp(n + 1, vector<int>(k + 1, INT_MAX));
+            for(int i = n - 1; i >= 0; i--) {
                 for(int p = 1; p <= k; p++) {
-                    int cost = dp[j + 1][j + 1][p - 1];
+                    int cost = front[j + 1][p - 1];
                     if(cost != INT_MAX) {
                         for(int t = i; t <= j; t++) {
                             cost += abs(houses[(i + j) / 2] - houses[t]);
                         }
                     }
-                    dp[i][j][p] = min(cost, dp[i][j + 1][p]);
+                    temp[i][p] = min(cost, front[i][p]);
                 }
             }
+            front = temp;
         }
-        return dp[0][0][k];
+        return front[0][k];
     }
 };
