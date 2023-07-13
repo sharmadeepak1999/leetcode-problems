614b298fb75600bcae96d2540d1648ce714e0002
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-07-13 20:51:53 +0530

    Time: 21 ms (85.18%), Space: 13.3 MB (83.94%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
+        vector<vector<int>> adj(numCourses);
         vector<int> indegree(numCourses, 0);
         for(auto &it:prerequisites) {
             adj[it[0]].push_back(it[1]);
             indegree[it[1]]++;
         }
-        int count = 0;
+        
         queue<int> q;
-        vector<int> visited(numCourses, 0);
+        
         for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) {
-                q.push(i);
-                visited[i] = 1;
-                count++;
-            }
+            if(indegree[i] == 0) q.push(i);
         }
         
+        int count = 0;
         while(!q.empty()) {
             int node = q.front();
             q.pop();
             
+            count++;
+            
             for(auto &adjNode:adj[node]) {
-                if(!visited[adjNode]) {
-                    indegree[adjNode]--;
-                    if(indegree[adjNode] == 0) {
-                        count++;
-                        q.push(adjNode);
-                    }
-                }
+                indegree[adjNode]--;
+                if(indegree[adjNode] == 0) q.push(adjNode);
             }
         }
         
