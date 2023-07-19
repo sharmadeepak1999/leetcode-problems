609e58c9d9703cc5d0ef987b3978a70cf0c1214d
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-07-19 18:03:59 +0530

    Time: 76 ms (17.92%), Space: 81.3 MB (11.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,31 +1,20 @@
 class Solution {
-public:
-    int helper(string s, string t, vector<vector<int>> &dp, int i, int j) {
-        if(j == 0) return i;
-        if(i == 0) return j;
+    int helper(string word1, string word2, int i, int j, vector<vector<int>> &dp) {
+        if(j == word2.size()) {
+            return word1.size() - i;
+        }
+        if(i == word1.size()) {
+            return word2.size() - j;
+        }
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i - 1] == t[j - 1]) {
-            return dp[i][j] = helper(s, t, dp, i - 1, j - 1);
-        } else {
-            return dp[i][j] = min(1 + helper(s, t, dp, i - 1, j - 1), min(1 + helper(s, t, dp, i - 1, j), 1 + helper(s, t, dp, i, j - 1)));
-        }
+        if(word1[i] == word2[j]) {
+            return dp[i][j] = helper(word1, word2, i + 1, j + 1, dp);
+        } 
+        return dp[i][j] = 1 + min(helper(word1, word2, i, j + 1, dp), min(helper(word1, word2, i + 1, j, dp), helper(word1, word2, i + 1, j + 1, dp)));
     }
-    int minDistance(string s, string t) {
-        int m = s.size();
-        int n = t.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        for(int i = 0; i <= m; i++) {
-            dp[i][0] = i;
-        }
-        for(int j = 0; j <= n; j++) {
-            dp[0][j] = j;
-        }
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1];
-                else dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
-            }
-        }
-        return dp[m][n];
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
+        return helper(word1, word2, 0, 0, dp);
     }
 };
