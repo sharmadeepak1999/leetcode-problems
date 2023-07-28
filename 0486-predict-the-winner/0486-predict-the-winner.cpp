aa18f0f6dc10ea1019239069cbc3a6a3cc36767a
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-07-28 12:03:08 +0530

    Time: 3 ms (70.47%), Space: 8.6 MB (8.42%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,23 +1,26 @@
 class Solution {
-    int helper(vector<int> &nums, int s, int e, int turn) {
+    int helper(vector<int> &nums, int s, int e, int turn, vector<vector<vector<int>>> &dp) {
         if(s > e) return 0;
+        if(dp[s][e][turn] != -1) return dp[s][e][turn];
         
-        int front = helper(nums, s + 1, e, !turn);
-        int back = helper(nums, s, e - 1, !turn);
+        int front = helper(nums, s + 1, e, !turn, dp);
+        int back = helper(nums, s, e - 1, !turn, dp);
         
         if(turn == 0) {
             front += nums[s];
             back += nums[e];
-            return max(front, back);
+            return dp[s][e][turn] = max(front, back);
         } else {
             front -= nums[s];
             back -= nums[e];
-            return min(front, back);
+            return dp[s][e][turn] = min(front, back);
         }
     }
 public:
     bool PredictTheWinner(vector<int>& nums) {
-        int score = helper(nums, 0, nums.size() - 1, 0);
+        int n = nums.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, -1)));
+        int score = helper(nums, 0, nums.size() - 1, 0, dp);
         return score >= 0;
     }
 };
