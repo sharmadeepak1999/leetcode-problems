b95aaefd55a7543f6bde4ed8c63e9923e458d7da
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2023-08-18 09:36:35 +0530

    Time: 47 ms (99.70%), Space: 30.6 MB (83.52%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<int> adj[n];
-        set<pair<int, int>> s;
         for(auto &road:roads) {
             adj[road[0]].push_back(road[1]);
             adj[road[1]].push_back(road[0]);
-            s.insert({ road[0], road[1] });
-            s.insert({ road[1], road[0] });
         }
         
         int maxi = 0;
@@ -15,7 +12,7 @@ public:
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
                 int common = 0;
-                if(s.find({ i, j }) != s.end()) {
+                if(find(adj[i].begin(), adj[i].end(), j) != end(adj[i])) {
                     common = 1;
                 }
                 int rank = adj[i].size() + adj[j].size() - common;
