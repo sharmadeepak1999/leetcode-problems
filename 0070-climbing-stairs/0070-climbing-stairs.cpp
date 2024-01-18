edc08a1289aaa01dcca4178d90ad4183e49646f5
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2024-01-18 12:54:35 +0530

    Time: 2 ms (45.07%), Space: 6.4 MB (59.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,36 +1,13 @@
 class Solution {
-public:
-    /*
-    int climbStairs(int n) {
-        if(n < 0) return 0;
-        if(n == 0) return 1;
-        
-        return climbStairs(n - 1) + climbStairs(n - 2);
-    }
-    */
-    /*
     int helper(int n, vector<int> &dp) {
         if(n < 0) return 0;
         if(n == 0) return 1;
-        
-        if(dp[n] != 0) return dp[n];
-        
-        return dp[n] = (helper(n - 1, dp) + helper(n - 2, dp));
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
     }
+public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1, 0);
+        vector<int> dp(n + 1, -1);
         return helper(n, dp);
-    }
-    */
-    
-    int climbStairs(int n) {
-        int one = 1, two = 1;
-        
-        for(int i = 2; i < n + 1; i++) {
-            int temp = one + two;
-            one = two;
-            two = temp;
-        }
-        return two;
     }
 };
