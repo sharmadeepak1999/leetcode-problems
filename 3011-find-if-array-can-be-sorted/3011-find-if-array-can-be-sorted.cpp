0171856596b2f638430ef00a5593323a71fe916a
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2024-11-06 15:33:43 +0530

    Time: 31 ms (8.63%), Space: 32.8 MB (16.22%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int noofbits(int n) {
+        int count = 0;
+        while(n) {
+            if(n % 2 == 1) count++;
+            n = n / 2;
+        }
+        return count;
+    }
+    bool canSortArray(vector<int>& nums) {
+        vector<int> temp = nums;
+        
+        sort(temp.begin(), temp.end());
+        
+        int s = 0, i = 1;
+        while(i < nums.size()) {
+            if(noofbits(nums[s]) != noofbits(nums[i])) {
+                cout << s << ' ' << i << endl;
+                sort(nums.begin() + s, nums.begin() + i);
+                s = i;
+            }
+            i++;
+        }
+        if(s < nums.size()) {
+            sort(nums.begin() + s, nums.end());
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != temp[i]) return false;
+        }
+        return true;
+    }
+};
