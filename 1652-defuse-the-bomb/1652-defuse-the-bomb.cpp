9975fc41ff5bff19cfcb5c77377ada03ce6cf4e7
Deepak Sharma <43602286+sharmadeepak1999@users.noreply.github.com>
2024-11-18 16:17:17 +0530

    Time: 26 ms (5.02%), Space: 10.4 MB (17.93%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        vector<int> temp(3 * code.size());
+        
+        for(int i = 1; i <= 3; i++) {
+            for(int j = 0; j < code.size(); j++) temp[code.size() * (i - 1) + j] = code[j];
+        }
+        
+        for(int i = 0; i < temp.size(); i++) cout << temp[i] << ' ';
+        cout << endl;
+        
+        vector<int> ans(code.size(), 0);
+        if(k == 0) return ans;
+        int s, e;
+        if(k < 0) {
+            s = code.size() + k;
+            e = code.size() - 1;
+        }
+        if(k > 0) {
+            s = code.size() + 1;
+            e = code.size() + k;
+        }
+        cout << s << ' ' << e << endl;
+        int winsum = accumulate(temp.begin() + s, temp.begin() + e + 1, 0);
+        
+        cout << winsum << endl;
+        for(int i = code.size(); i < 2 * code.size(); i++) {
+            ans[i - code.size()] = winsum;
+            winsum -= temp[s];
+            winsum += temp[e + 1];
+            s++, e++;
+        }
+        return ans;
+    }
+};
